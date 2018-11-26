1243da37cc843ef564f789a0e59a93c92e7015d4
Martin Grimmer <grimmer@informatik.uni-leipzig.de>
2018-11-26 14:59:52 +0100

    renamed folder src -> lidds, renamed some string to lidds in setup.py and Makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 clean:
 	rm -rf lid.egg-info && rm -rf dist && rm -rf build
 
-build: *.py src/*.py clean
+build: *.py lidds/*.py clean
 	source ./scripts/updateVersionBuild.sh && python3 setup.py sdist bdist_wheel
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='lid_sim',
+setup(name='lidds',
       version='0.1.28',
-      description='Leipzig Intrusion Detection Framework',
+      description='Leipzig Intrusion Detection Data Set Framework',
       url='https://github.com/LID-DS/LID-DS',
-      packages=['src'],
+      packages=['lidds'],
       platforms=['linux_x86_64'],
       install_requires=[
           'argparse',
