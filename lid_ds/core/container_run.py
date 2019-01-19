ec5acc65845b960bfb00e60cdbc796aa09dbeb2b
Dennis Kreußel <dennis.kreussel94@gmail.com>
2019-01-19 12:57:28 +0100

    Implement core recording requirements

---
+++ lid_ds/core/container_run.py
@@ -0,0 +1,29 @@
+"""
+container run aims to provide a context manager for sanely
+starting and stopping of containers
+"""
+from contextlib import contextmanager
+from docker import from_env
+from lid_ds.helpers import wait_until
+
+@contextmanager
+def container_run(config, check_if_available=lambda container: True):
+    """
+    A contextmanager for container ressource management
+    handles starting, stopping and removing of containers
+    """
+    docker_client = from_env()
+    if ('image_name' in config and
+            isinstance(config['image_name'], str) and
+            'port_mapping' in config and
+            isinstance(config['port_mapping'], dict)):
+        container = docker_client.containers.run(
+            config['image_name'],
+            detach=True,
+            stdin_open=True,
+            tty=True,
+            ports=config['port_mapping']
+            )
+        wait_until(check_if_available, 60, 1, container=container)
+    yield container
+    container.remove(force=True)
---
+++ lid_ds/core/scenario.py
@@ -0,0 +1,107 @@
+"""
+The purpose of the Scenario module is to provide the scenario class.
+The scenario class should give a libraryuser the ability to simply
+create new scenarios and implementing needed functions.
+"""
+import logging
+
+from abc import ABCMeta, abstractmethod
+from threading import Thread, Timer
+from time import sleep
+from lid_ds.helpers import scenario_name
+from .container_run import container_run
+from .recorder_run import record_container
+
+logger = logging.getLogger(__name__)
+
+class Scenario(metaclass=ABCMeta):
+    """
+    The scenario class provides a baseclass to derive from
+    in order to implement a custom security scenario
+    """
+    def __init__(
+            self,
+            image_name,
+            port_mapping={},
+            warmup_time=60,
+            recording_time=300,
+            behaviours=[],
+            **kwargs
+    ):
+        """
+        initialize all time sequences needed for the recording process
+        as well es for statistically relevant execution
+        """
+        self.image_name = image_name
+        self.port_mapping = port_mapping
+        self.warmup_time = warmup_time
+        self.behaviours = behaviours
+        self.recording_time = recording_time
+        self.execute_exploit = 'exploit_start_time' in kwargs
+        if not isinstance(self.warmup_time, (int, float)):
+            raise TypeError("Warmup time needs to be an integer or float")
+        if not isinstance(self.recording_time, (int, float)):
+            raise TypeError("Recording time needs to be an integer or float")
+        if self.execute_exploit:
+            self.exploit_start_time = kwargs.get('exploit_start_time')
+            if not isinstance(self.exploit_start_time, (int, float)):
+                raise TypeError("Exploit start time needs to be an integer or float")
+
+            if self.exploit_start_time > recording_time:
+                raise ValueError(
+                    "The start time of the exploit must be before the end of the recording!"
+                    )
+        self.current_threads = []
+
+        self.name = scenario_name(self)
+
+    def __call__(self, with_exploit=False):
+        print('Simulating Scenario: {}'.format(self))
+        with container_run({
+            'image_name': self.image_name,
+            'port_mapping': self.port_mapping
+        }, self.wait_for_availability) as container:
+            print('Start Normal Behaviours for Scenario: {}'.format(self.name))
+            for behaviour in self.behaviours:
+                thread_behaviour = Thread(target=behaviour, args=())
+                thread_behaviour.start()
+                self.current_threads.append(thread_behaviour)
+            print('Warming up Scenario: {}'.format(self.name))
+            sleep(self.warmup_time)
+            print('Start Recording Scenario: {}'.format(self.name))
+            with record_container(container, self.name) as recorder:
+                # Do normal behaviour
+                if self.execute_exploit:
+                    print('Start Exploiting Scenario: {}'.format(self.name))
+                    Timer(self.exploit_start_time, self.exploit, (container))
+                sleep(self.recording_time)
+
+    def __repr__(self):
+        if self.execute_exploit:
+            return '<{} {} recording_time={} warmup_time={} exploit_start_time={}>'.format(
+                self.__class__.__name__,
+                self.name,
+                self.recording_time,
+                self.warmup_time,
+                self.exploit_start_time
+                )
+        return '<{} {} recording_time={} warmup_time={}>'.format(
+            self.__class__.__name__,
+            self.name,
+            self.recording_time,
+            self.warmup_time,
+            )
+
+    @abstractmethod
+    def exploit(self, container):
+        """
+        Implement the exploit method in the derived class
+        to add a hook that gets called when the exploit shall
+        be executed.
+        """
+    @abstractmethod
+    def wait_for_availability(self, container):
+        """
+        Implement a hook that returns once the container is ready
+        """
+
