5bc6b5d7ace154e2e5c3de1a72f9489ad3b926b1
Dennis Kreußel <dennis.kreussel94@gmail.com>
2019-01-19 12:59:13 +0100

    Implement data models for analyzing recordings

---
+++ lid_ds/data_models/syscall.py
@@ -0,0 +1,84 @@
+from terminaltables import AsciiTable
+
+class SysCall():
+    _repr_header = [
+            'system call',
+            'process_id',
+            'thread_id',
+            'cpu',
+            'duration',
+            'args_len',
+            'res_len'
+        ]
+
+    def __init__(self, start_event, stop_event):
+        self.start_event = start_event
+        self.stop_event = stop_event
+
+    @property
+    def type(self):
+        return self.start_event.event_type
+
+    @property
+    def process(self):
+        return self.start_event.process
+
+    @property
+    def executing_cpu(self):
+        return self.start_event.executing_cpu
+
+    @property
+    def thread_id(self):
+        return self.start_event.thread_id
+
+    @property
+    def duration(self):
+        return self.stop_event.event_time - self.start_event.event_time
+
+    @property
+    def start_timestamp(self):
+        return self.start_event.event_time
+
+    @property
+    def end_timestamp(self):
+        return self.stop_event.event_time
+
+    @property
+    def args(self):
+        return self.start_event.args
+
+    @property
+    def res(self):
+        return self.stop_event.args
+
+    def _get_table_rep_row(self):
+        return [
+            self.type,
+            self.process,
+            str(self.thread_id),
+            str(self.executing_cpu),
+            str(self.duration.total_seconds()),
+            str(len(self.args)),
+            str(len(self.res))
+        ]
+    def __repr__(self):
+        table_data = []
+        table_data.append(SysCall._repr_header)
+        table_data.append(self._get_table_rep_row())
+        table = AsciiTable(table_data)
+        table.title = 'SysCall ({})'.format(self.start_timestamp)
+        return table.table
+
+    @staticmethod
+    def summary(syscalls):
+        table_data = []
+        table_data.append(SysCall._repr_header)
+        for syscall in syscalls:
+            table_data.append(syscall._get_table_rep_row())
+        table = AsciiTable(table_data)
+        table.title = 'System Calls ({})'.format(len(syscalls))
+        return table.table
+
+
+
+
---
+++ lid_ds/data_models/sysdig_event.py
@@ -0,0 +1,49 @@
+import re
+from datetime import datetime
+from terminaltables import AsciiTable
+
+class SysdigEvent():
+    _repr_header = [
+            'system call',
+            'process_id',
+            'thread_id',
+            'cpu',
+            'event_time',
+            'begin_event',
+            'args'
+        ]
+
+    def __init__(self, sysdig_line):
+        tokens = sysdig_line.decode("utf-8").split()
+
+        self.sysdig_recording_index = int(tokens[0])
+        self.event_time = datetime.strptime(tokens[1][:-3], '%H:%M:%S.%f')
+        self.executing_cpu = int(tokens[2])
+        self.process = tokens[3]
+
+        thread_id_regex = re.compile('([()])')
+        self.thread_id = re.sub(thread_id_regex, '', tokens[4])
+
+        self.enter_event = True if tokens[5] == '>' else False
+
+        self.event_type = tokens[6]
+
+        self.args = tokens[7:]
+        #print('{} {} {} {} {} {} {}'.format(sysdig_recording_index, event_time, executing_cpu, process, thread_id, enter_event, event_type))
+
+    def __repr__(self):
+        table_data = []
+        table_data.append(SysdigEvent._repr_header)
+        repr_data = [
+            self.event_type,
+            self.process,
+            str(self.thread_id),
+            str(self.executing_cpu),
+            str(self.event_time),
+            str(self.enter_event),
+            len(self.args)
+        ]
+        table_data.append(repr_data)
+        table = AsciiTable(table_data)
+        table.title = 'Sysdig Event #{}'.format(self.sysdig_recording_index)
+        return table.table
