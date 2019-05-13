18103fc01fed15750d2555bbc8fb24e1c275b3dc
Martin Grimmer <mgrimmer42@gmail.com>
2019-05-13 10:00:17 +0200

    added normal behaviour to example scenario

--- example/main.py
+++ example/main.py
@@ -1,4 +1,3 @@
-import requests
 import sys
 import json
 import base64
@@ -11,17 +10,21 @@ from requests.auth import HTTPBasicAuth
 from lid_ds.core import Scenario
 from lid_ds.sim import Behaviour
 
-warmt = int(sys.argv[1])
-rect = int(sys.argv[2])
+warmup_time = int(sys.argv[1])
+recording_time = int(sys.argv[2])
+total_duration = warmup_time + recording_time
+exploit_time = random.randint(int(recording_time * .3), int(recording_time * .8))
+print("Exploit time : {}".format(str(exploit_time)))
 
+
+db_name = "textDB"
 min_user_count = 10
 max_user_count = 25
 user_count = random.randint(min_user_count, max_user_count)
 
-total_duration = warmt+rect
-warmup_time = warmt
-exploit_time = random.randint(int(rect * .3), int(rect * .8))
-print("Exploit time : {}".format(str(exploit_time)))
+words = open('/etc/dictionaries-common/words').read().splitlines()
+
+host = "localhost"
 
 class CVE_2012_2122(Scenario):
     def exploit(self, container):
@@ -33,55 +36,81 @@ class CVE_2012_2122(Scenario):
 
     def wait_for_availability(self, container):
         try:
-            db = pymysql.connect("localhost", "root", "123456")
+            db = pymysql.connect(host, "root", "123456")
         except Exception:
             print('MySQL Server is still down!')
             return False
         print('MySQL server is up - we can start simulating users!')
         return True
 
+
 class MySQLUser(Behaviour):
-    def __init__(self, host, uname, passwd, total_duration):
-        super().__init__([], total_duration)
+    def __init__(self, host, uname, passwd, rec_time):
+        super().__init__([], rec_time)
         self.host = host
         self.uname = uname
         self.passwd = passwd
         self.actions.append(self._init_normal)
-        for wait_time in self.wait_times[1:]:
+        print("Behavior with actions: " + str(len(self.wait_times)))
+        for wt in self.wait_times[1:]:
             self.actions.append(self.do_normal)
 
     def _init_normal(self):
         try:
-            self.db = pymysql.connect("localhost", uname, passwd)
+            self.db = pymysql.connect(self.host, self.uname, self.passwd)
             try:
-                self.db.cursor().execute('create database testdb')
-            except Exception as e:
-                print(e)
-            self.db = pymysql.connect("localhost", uname, passwd, "testdb")
-        except:
+                self.db.cursor().execute('create database ' + db_name)
+            except:
+                pass
+
+            self.db = pymysql.connect(self.host, self.uname, self.passwd, db_name)
+            sql = """CREATE TABLE `texts` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT, 
+                         `text` varchar(255) COLLATE utf8_bin NOT NULL, 
+                         PRIMARY KEY (`id`)
+                     ) 
+                     ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin 
+                     AUTO_INCREMENT=1 ;"""
+            self.db.cursor().execute(sql)
+        except Exception as e:
             pass
 
     def do_normal(self):
         try:
-            pass
-        except Exception as Error:
-            pass
-            #print(Error)
+            if random.random() > 0.5:
+                word = random.choice(words).replace("'", "")
+                sql = "INSERT INTO `texts` (`text`) VALUES ('" + word + "');"
+                print("Insert: " + word )
+                with self.db.cursor() as cursor:
+                    self.db.begin()
+                    cursor.execute(sql)
+                self.db.commit()
+            else:
+                sql = "SELECT * FROM `texts` ORDER BY RAND() LIMIT 1"
+                with self.db.cursor() as cursor:
+                    cursor.execute(sql)
+                result = cursor.fetchone()
+                print("Got: " + result[1])
+        except Exception as e:
+            print("Exception: " + str(e))
+            sys.exit()
+            #try:
+            #    self.db = pymysql.connect(self.host, self.uname, self.passwd, db_name)
+            #except:
+
 
 behaviours = []
 for i in range(user_count):
-    duration = random.random() * total_duration
-    behaviours.append(MySQLUser("localhost", "root", "123456", total_duration * (i/user_count)))
-    print('creating {}.th user with duration: {}'.format(i, duration))
+    behaviours.append(MySQLUser("localhost", "root", "123456", recording_time))
 
 scenario = CVE_2012_2122(
-        'vulhub/mysql:5.5.23',
-        port_mapping={
-            '3306/tcp' : 3306
-        },
-        warmup_time=warmup_time,
-        recording_time=(total_duration-warmup_time),
-        behaviours=behaviours,
-        exploit_start_time=exploit_time # Comment this line if you don't want the exploit to be executed
-    )
+    'vulhub/mysql:5.5.23',
+    port_mapping={
+        '3306/tcp': 3306
+    },
+    warmup_time=warmup_time,
+    recording_time=recording_time,
+    behaviours=behaviours,
+    exploit_start_time=exploit_time  # Comment this line if you don't want the exploit to be executed
+)
 scenario()
