9d504e47c4222aeec7678e07106ecd0eadda3d58
Toni Rucks <zerotank@hotmail.de>
2020-01-23 17:56:12 +0100

    adds tcpdump

--- lid_ds/core/recorder_run.py
+++ lid_ds/core/recorder_run.py
@@ -7,6 +7,18 @@ import signal
 import pexpect
 from contextlib import contextmanager
 
+
+def kill_child(child):
+    pid = child.pid
+    tries = 0
+    while child.isalive():
+        tries += 1
+        child.sendcontrol('c')
+        if tries > 1000:
+            break
+    if child.isalive():
+        os.kill(pid, signal.SIGINT)
+
 @contextmanager
 def record_container(container, recording_name, buffer_size=80):
     """
@@ -14,16 +26,12 @@ def record_container(container, recording_name, buffer_size=80):
     process for the lifetime of the container
     """
     out_dir = os.environ.get('LIDDS_OUT_DIR', '.')
-    out_path = os.path.join(out_dir, '{}.scap'.format(recording_name))
-    print('Saving to {}'.format(out_path))
-    child = pexpect.spawn('sysdig -w {} -s {} container.name={} --unbuffered'.format(out_path, buffer_size, container.name))
-    yield child
-    pid = child.pid
-    tries = 0
-    while child.isalive():
-        tries+=1
-        child.sendcontrol('c')
-        if tries > 1000:
-            break
-    if child.isalive():
-        os.kill(pid, signal.SIGINT)
+    sysdig_out_path = os.path.join(out_dir, '{}.scap'.format(recording_name))
+    tcp_out_path = os.path.join(out_dir, '{}.pcap'.format(recording_name))
+    print('Saving to Sysdig to {}'.format(sysdig_out_path))
+    sysdig_child = pexpect.spawn('sysdig -w {} -s {} container.name={} --unbuffered'.format(sysdig_out_path, buffer_size, container.name))
+    print('Saving to TCPDump to {}'.format(tcp_out_path))
+    tcp_child = pexpect.spawn('tcpdump -i docker0 -w {}'.format(tcp_out_path))
+    yield sysdig_child, tcp_child
+    kill_child(tcp_child)
+    kill_child(sysdig_child)
