8f931d67c5df65edf6193331d4c9f3c8e8bd83d6
Nirsi <felix@nirsberger.de>
2021-04-19 17:12:27 +0200

    quickfix of stdin invoke of exploit image

--- lid_ds/core/objects/attacker.py
+++ lid_ds/core/objects/attacker.py
@@ -1,11 +1,12 @@
 import secrets
 import time
+from threading import Thread
 
 from lid_ds.core.collector.collector import Collector
 from lid_ds.core.image import Image, ChainImage
 from lid_ds.utils.docker_utils import format_command, get_ip_address
 from lid_ds.core.objects.base import ScenarioContainerBase
-from lid_ds.sim.dockerize import run_image
+from lid_ds.sim.dockerize import run_image, show_logs
 from lid_ds.utils import log
 
 
@@ -15,10 +16,15 @@ class ScenarioAttacker(ScenarioContainerBase):
         self.container = None
         self.container_name = secrets.token_hex(8)
         self.logger = log.get_logger(f"[ATTACKER] {self.container_name}", self.queue)
+        self.logging_thread = None
 
     def start_container(self):
-        self.container = run_image(self.image.name, self.network, self.container_name)
+        args = format_command(self.image.init_args)
+        self.container = run_image(self.image.name, network=self.network, name=self.container_name, command=args)
         Collector().add_container(self.container_name, "attacker", get_ip_address(self.container))
+        if self.to_stdin:
+            self.logging_thread = Thread(target=show_logs, args=(self.container, self.logger))
+            self.logging_thread.start()
 
     def execute_exploit_at_time(self, execution_time):
         while time.time() < execution_time:
@@ -42,3 +48,5 @@ class ScenarioAttacker(ScenarioContainerBase):
 
     def teardown(self):
         self.container.stop()
+        if self.logging_thread:
+            self.logging_thread.join()
