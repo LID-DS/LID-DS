0079a5d6518e6550047e64bf669cb5b174b5f88f
boneymoy88 <38049744+boneymoy88@users.noreply.github.com>
2021-09-24 14:13:23 +0200

    #105 dataloader (#158)
    * initial commit for ticket #105
    
    * advanced with dataloader. pcap inside zip not working yet
    
    * packet extractor returns Extractor object
    
    * deleted example recording
    
    * small formatting changes
    
    Co-authored-by: Martin Grimmer <mgrimmer42@gmail.com>

---
+++ Tools/data_loader.py
@@ -0,0 +1,231 @@
+import os
+import glob
+import json
+import zipfile
+from enum import Enum
+from recording import Recording
+
+TRAINING = 'training'
+VALIDATION = 'validation'
+TEST = 'test'
+
+
+class RecordingType(Enum):
+    NORMAL = 1
+    NORMAL_AND_ATTACK = 2
+    ATTACK = 3
+    IDLE = 4
+
+
+def get_file_name(path: str) -> str:
+    """
+        Return file name without path and extension
+
+        Parameter:
+        path (str): path of file
+
+        Returns:
+        str: file name
+
+    """
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+class DataLoader:
+    """
+
+        Recieves path of scenario.
+
+        Args:
+        scenario_path (str): path of scenario folder
+
+        Attributes:
+        scenario_path (str): stored Arg
+        metadata_list (list): list of metadata for each recording
+
+    """
+
+    def __init__(self, scenario_path):
+        """
+
+            Save path of scenario and create metadata_list.
+
+            Parameter:
+            scenario_path (str): path of assosiated folder
+
+        """
+        self.scenario_path = scenario_path
+        self.metadata_list = self.collect_metadata()
+
+    def training_data(self, recording_type: RecordingType = None) -> list:
+        """
+
+            Create list of recordings contained in training data.
+            Specify recordings with recording_type.
+
+            Parameter:
+            recording_type (RecordingType): only include recordings of recording_type
+                :default: all included
+
+            Returns:
+            list: list of training data recordings
+
+        """
+        recordings = self.extract_recordings(category=TRAINING,
+                                             recording_type=recording_type)
+        return recordings
+
+    def validation_data(self, recording_type: RecordingType = None) -> list:
+        """
+
+            Create list of recordings contained in validation data.
+            Specify recordings with recording_type.
+
+            Parameter:
+            recording_type (RecordingType): only include recordings of recording_type
+                :default: all included
+
+            Returns:
+            list: list of validation data recordings
+
+        """
+        recordings = self.extract_recordings(category=VALIDATION,
+                                             recording_type=recording_type)
+        return recordings
+
+    def test_data(self, recording_type: RecordingType = None) -> list:
+        """
+
+            Create list of recordings contained in test data.
+            Specify recordings with recording_type.
+
+            Parameter:
+            recording_type (RecordingType): only include recordings of recording_type
+                :default: all included
+
+            Returns:
+            list: list of test data recordings
+
+        """
+        recordings = self.extract_recordings(category=TEST,
+                                             recording_type=recording_type)
+        return recordings
+
+    def extract_recordings(self,
+                           category: str,
+                           recording_type: RecordingType = None) -> list:
+        """
+
+            Go through list of all files in specified category.
+            Instanciate new Recording object and append to recordings list.
+            If all files have been seen return list of Recordings.
+
+            Parameter:
+            category (str): filter for category (training, validation, test)
+            recording_type (RecordingType): only include recordings of recording_type
+                :default: all included
+
+            Returns:
+            list: list of data recordings for specified category
+
+
+        """
+        recordings = []
+        file_list = sorted(self.metadata_list[category].keys())
+        for file in file_list:
+            # check filter
+            if recording_type:
+                if self.metadata_list[category][file]['recording_type'] == recording_type:
+                    recordings.append(Recording(name=file,
+                                                path=self.metadata_list[category][file]['path']))
+            else:
+                recordings.append(Recording(name=file,
+                                            path=self.metadata_list[category][file]['path']))
+        return recordings
+
+    def collect_metadata(self) -> dict:
+        """
+
+            Create dictionary which contains following information about recording:
+                first key: Category of recording : training, validataion, test
+                second key: Name of recording
+                value : {recording type: str, path: str}
+
+            Returns:
+            dict: metadata_dict containing type of recording for every recorded file
+
+        """
+        metadata_dict = {
+            'training': {},
+            'validation': {},
+            'test': {}
+        }
+        training_files = glob.glob(self.scenario_path + f'/{TRAINING}/*.zip')
+        val_files = glob.glob(self.scenario_path + f'/{VALIDATION}/*.zip')
+        test_files = glob.glob(self.scenario_path + f'/{TEST}/*/*.zip')
+        # create list of all files
+        all_files = training_files + val_files + test_files
+        for file in all_files:
+            with zipfile.ZipFile(file, 'r') as zip_ref:
+                # remove zip extension and create json file name
+                json_file_name = get_file_name(file) + '.json'
+                with zip_ref.open(json_file_name) as unzipped:
+                    unzipped_byte_json = unzipped.read()
+                    unzipped_json = json.loads(unzipped_byte_json.decode('utf8'))
+                    recording_type = self.get_type_of_recording(unzipped_json)
+                    temp_dict = {
+                        'recording_type': recording_type,
+                        'path': file
+                    }
+                    if TRAINING in os.path.dirname(file):
+                        metadata_dict[TRAINING][get_file_name(file)] = temp_dict
+                    elif VALIDATION in os.path.dirname(file):
+                        metadata_dict[VALIDATION][get_file_name(file)] = temp_dict
+                    elif TEST in os.path.dirname(file):
+                        metadata_dict[TEST][get_file_name(file)] = temp_dict
+                    else:
+                        raise TypeError()
+        return metadata_dict
+
+    def get_type_of_recording(self, json_dict: dict) -> RecordingType:
+        """
+
+            Receives json dict and determines the recording type.
+
+            Parameter:
+            json_dict (dict): json including metadata
+
+            Returns:
+            RecordingType: Enumeration describing type
+
+        """
+        data = json_dict
+
+        normal_behavoiur = False
+        exploit = False
+
+        # check for normal behaviour:
+        for container in data["container"]:
+            if container["role"] == "normal":
+                normal_behavoiur = True
+                break
+        # check for exploit
+        if data["exploit"]:
+            exploit = True
+
+        if normal_behavoiur is False and exploit is False:
+            return RecordingType.IDLE
+        if normal_behavoiur is False and exploit is True:
+            return RecordingType.ATTACK
+        if normal_behavoiur is True and exploit is False:
+            return RecordingType.NORMAL
+        if normal_behavoiur is True and exploit is True:
+            return RecordingType.NORMAL_AND_ATTACK
+
+
+if __name__ == "__main__":
+    dataloader = DataLoader('../../Bruteforce')
+    training_data = dataloader.training_data()
+    for recording in training_data:
+        print("done")
+        pck = recording.packets()
---
+++ Tools/recording.py
@@ -0,0 +1,155 @@
+import os
+import csv
+import json
+import pcapkit
+import zipfile
+
+
+class Recording:
+    """
+
+        Single recording captured in 4 ways
+        class provides functions to handle every type of recording
+            --> syscall text file
+            --> pcap packets
+            --> json describing recording
+            --> statistics of resources
+
+        Args:
+        path (str): path of recording
+        name (str): name of file without extension
+
+    """
+
+    def __init__(self, path: str, name: str):
+        """
+
+            Save name and path of recording.
+
+            Parameter:
+            path (str): path of assosiated files
+            name (str): name without path and extension
+
+        """
+        self.path = path
+        self.name = name
+        pass
+
+    def syscalls(self) -> str:
+        """
+
+            Prepare stream of syscalls,
+            yield single lines
+
+            Returns:
+            str: syscall text line
+
+        """
+        with zipfile.ZipFile(self.path, 'r') as zipped:
+            with zipped.open(self.name + '.sc') as unzipped:
+                for syscall in unzipped:
+                    yield syscall.decode('utf-8').rstrip()
+
+    def packets(self):
+        """
+
+            Unzip and extract pcap objects,
+
+            Returns:
+            pcap obj: return pypcap Extractor object
+            src:
+                https://pypcapkit.jarryshaw.me/en/latest/foundation/extraction.html#pcapkit.foundation.extraction.Extractor
+
+        """
+        try:
+            with zipfile.ZipFile(self.path, 'r') as zipped:
+                file_list = zipped.namelist()
+                for file in file_list:
+                    if file.endswith('.pcap'):
+                        zipped.extract(file, 'tmp')
+            obj = pcapkit.extract(fin=f'tmp/{self.name}.pcap',
+                                  engine='scapy',
+                                  store=True,
+                                  nofile=True,
+                                  tcp=True,
+                                  strict=True)
+        except Exception:
+            print(f'Error extracting pcap file {self.name}')
+            return None
+        finally:
+            os.remove(f'tmp/{self.name}.pcap')
+
+        return obj
+
+    def resource_stats(self) -> list:
+        """
+
+            Read .res file of recording.
+            Includes usage of following resources for a point in time:
+                timestamp,
+                cpu_usage,
+                memory_usage,
+                network_received,
+                network_send,
+                storage_read,
+                storage_written
+
+            Returns:
+            List of used resources
+
+        """
+        statistics = []
+        with zipfile.ZipFile(self.path, 'r') as zipped:
+            with zipped.open(self.name + '.res') as unzipped:
+                string = unzipped.read().decode('utf-8')
+                reader = csv.reader(string.split('\n'), delimiter=',')
+                # remove header
+                next(reader)
+                for row in reader:
+                    statistics.append(row)
+        return statistics
+
+    def metadata(self) -> dict:
+        """
+
+            Read json file and extract metadata as dict
+            with following format:
+            {"container": [
+                    "ip": str,
+                    "name": str,
+                    "role": str
+             "exploit": bool,
+             "exploit_name": str,
+             "image": str,
+             "recording_time": int,
+             "time":{
+                    "container_ready": {
+                        "absolute": float,
+                        "relative": float,
+                        "source": str
+                    },
+                    "exploit": [
+                        {
+                            "absolute": float,
+                            "name": str,
+                            "relative": float,
+                            "source": str
+                        }
+                    ]
+                    "warmup_end": {
+                        "absolute": float,
+                        "relative": float,
+                        "source": str
+                    }
+                }
+            }
+
+            Returns:
+            dict: metadata dictionary
+
+        """
+        with zipfile.ZipFile(self.path, 'r') as zipped:
+            with zipped.open(self.name + '.json') as unzipped:
+                unzipped_byte_json = unzipped.read()
+                unzipped_json = json.loads(unzipped_byte_json.decode('utf-8').replace("'", '"'))
+        return unzipped_json
