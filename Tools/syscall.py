70dd925516ea65257c58556292bcc600034a3105
Felix Nirsberger <felix@nirsberger.de>
2021-09-29 14:03:09 +0200

    added type hints and return values

--- Tools/syscall.py
+++ Tools/syscall.py
@@ -26,7 +26,6 @@ class Param(IntEnum):
 
 class Syscall:
     """
-
     represents one system call as an object created from a linestring out of an LID-DS 2021 recording
     features lazy instantiation of syscall attributes
 
@@ -34,7 +33,7 @@ class Syscall:
 
     """
 
-    def __init__(self, syscall_line):
+    def __init__(self, syscall_line: str):
         self.syscall_line = syscall_line
         self._line_list = syscall_line.split(' ')
         self._timestamp_unix = None
@@ -47,10 +46,13 @@ class Syscall:
         self._direction = None
         self._params = None
 
-    def timestamp_unix_in_ns(self):
+    def timestamp_unix_in_ns(self) -> int:
         """
 
         casts unix timestamp from string to int
+
+        Returns:
+            int: unix timestamp of syscall
 
         """
         if self._timestamp_unix is None:
@@ -58,10 +60,13 @@ class Syscall:
 
         return self._timestamp_unix
 
-    def timestamp_datetime(self):
+    def timestamp_datetime(self) -> datetime:
         """
 
         casts unix timestamp from string to python datetime object
+
+        Returns:
+            datetime: casted datetime object of syscall timestamp
 
         """
         if self._timestamp_datetime is None:
@@ -70,21 +75,26 @@ class Syscall:
 
         return self._timestamp_datetime
 
-    def user_id(self):
+    def user_id(self) -> int:
         """
 
         casts user_id from string to int
 
+        Returns:
+            int: user id
         """
         if self._user_id is None:
             self._user_id = int(self._line_list[SyscallSplitPart.USER_ID])
 
         return self._user_id
 
-    def process_id(self):
+    def process_id(self) -> int:
         """
 
         casts process_id from string to int
+
+        Returns:
+            int: process id
 
         """
         if self._process_id is None:
@@ -92,10 +102,13 @@ class Syscall:
 
         return self._process_id
 
-    def process_name(self):
+    def process_name(self) -> str:
         """
 
-        casts process_id from string to int
+        extracts process name
+
+        Returns:
+            string: process Name
 
         """
         if self._process_name is None:
@@ -103,10 +116,13 @@ class Syscall:
 
         return self._process_name
 
-    def thread_id(self):
+    def thread_id(self) -> int:
         """
 
         casts thread_id from string to int
+
+        Returns:
+            int: thread id
 
         """
         if self._thread_id is None:
@@ -114,10 +130,13 @@ class Syscall:
 
         return self._thread_id
 
-    def name(self):
+    def name(self) -> str:
         """
 
         gets syscall name from recorded line
+
+        Returns:
+            string: syscall name
 
         """
         if self._name is None:
@@ -125,10 +144,13 @@ class Syscall:
 
         return self._name
 
-    def direction(self):
+    def direction(self) -> Direction:
         """
 
         sets direction based on chars '<' and '>', casts to OPEN/CLOSE in enum
+
+        Returns:
+            Direction: the direction of the syscall
 
         """
         if self._name is None:
@@ -140,10 +162,13 @@ class Syscall:
 
         return self._direction
 
-    def params(self):
+    def params(self) -> dict:
         """
 
         extracts params from param list and saves its names and values as dict
+
+        Returns:
+            dict: the syscalls parameters
 
         """
         if self._params is None:
@@ -154,11 +179,14 @@ class Syscall:
                     self._params[split[Param.NAME]] = split[Param.VALUE]
         return self._params
 
-    def param(self, param_name, b64decode=False):
+    def param(self, param_name: str, b64decode: bool = False) -> (bytes, str):
         """
 
         runs the params() method and returns the requested
         decodes base64 strings if activated
+
+        Returns:
+            str or bytes: syscall parameter value
 
         """
         params = self.params()
