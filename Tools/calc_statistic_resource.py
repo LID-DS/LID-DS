f38a176fdd09065cd7c28dc56474d202e8819c15
tk <tim_k@posteo.de>
2021-10-04 11:56:28 +0200

    finished calculation of resources

--- Tools/calc_statistic_resource.py
+++ Tools/calc_statistic_resource.py
@@ -1,9 +1,28 @@
+import os
 import json
+from tqdm import tqdm
+import argparse
 from typing import Union
 
 from Tools.data_loader import DataLoader
-from Tools.data_loader import get_type_of_recording
 from Tools.data_loader import RecordingType
+
+"""
+{
+    <scenario_name> {
+        <dataset_part> {
+            <recording_type> {
+                cpu_usage: float,
+                memory_usage: float,
+                network_send: int,
+                network_received: int,
+                storage_read: int,
+                storage_written: int
+
+        }
+    }
+}
+"""
 
 
 SCENARIO_NAMES = [
@@ -24,29 +43,27 @@ SCENARIO_NAMES = [
     "ZipSlip"
 ]
 
-stats_per_scenario = {
-    "Bruteforce_CWE-307": {},
-    "CVE-2012-2122": {},
-    "CVE-2014-0160": {},
-    "CVE-2017-7529": {},
-    "CVE-2017-12635_6": {},
-    "CVE-2018-3760": {},
-    "CVE-2019-5418": {},
-    "CVE-2020-9484": {},
-    "CVE-2020-13942": {},
-    "CVE-2020-23839": {},
-    "CWE-89-SQL-Injection": {},
-    "EPS_CWE-434": {},
-    "Juice-Shop": {},
-    "PHP_CWE-434": {},
-    "ZipSlip": {}
-}
+
+def save_to_json(results: dict, output_path: str, scenario_name: str):
+    """
+    saves results for one scenario to json file located at a given path
+    overwrites old files
+    """
+    with open(os.path.join(output_path, scenario_name + '_stats.json'), 'w') as jsonfile:
+        json.dump(results, jsonfile, indent=4)
 
 
 def update_stat_single(current: Union[int, float], stat: dict) -> dict:
     """
 
-        receives current value and updates min max and avg
+        receives current value and updates min max and avg of specific statistic of resources
+
+        Param:
+        current (int, float): current value for specific resource
+        stat (dict): statistic of previous resource
+
+        Returns:
+        dict: updated statistic of resource
 
     """
     if stat['min']:
@@ -61,6 +78,19 @@ def update_stat_single(current: Union[int, float], stat: dict) -> dict:
 
 
 def update_stat(stats_recording: dict, resource: Union[int, float]) -> dict:
+    """
+
+        update current statstic of resources of single recording (stats_recording)
+        with entry of resource
+
+        Param:
+        stats_recording (dict): current statistic of single recording
+        resource (int, float): current resource entry for timestamp
+
+        Returns:
+        dict: updated statistic of single recording
+
+    """
     stats_recording['cpu_usage'] = update_stat_single(current=resource.cpu_usage(),
                                                       stat=stats_recording['cpu_usage'])
     stats_recording['memory_usage'] = update_stat_single(current=resource.memory_usage(),
@@ -76,7 +106,15 @@ def update_stat(stats_recording: dict, resource: Union[int, float]) -> dict:
     return stats_recording
 
 
-def set_default_stat():
+def set_default_stat() -> dict:
+    """
+
+        helper function to set specific initial values
+
+        Returns:
+        dict: with set initial values
+
+    """
     stats_recording = {
         'cpu_usage': {
             'min': None,
@@ -118,114 +156,130 @@ def set_default_stat():
     return stats_recording
 
 
-def stats_per_category(data, category: str, stats_per_category: dict) -> dict:
-    stats_recording = set_default_stat()
-    if category == 'test':
-        tmp_dict = {
-            'ATTACK': [],
-            'IDLE': [],
-            'NORMAL': [],
-            'NORMAL_AND_ATTACK': []
-        }
-    else:
-        tmp_dict = {
-            'IDLE': [],
-            'NORMAL': [],
-        }
-    for recording in data:
+def calc_stats_for_recording_type(recording_list: list, description) -> dict:
+    """
+
+        calculates statsitic for whole recording_type
+
+        Param:
+        recording_list (list): list if Recordings
+
+        Rerturns:
+        dict: statistic of recording_type
+
+    """
+    # calc for every recording
+    resources_per_recording = []
+    for recording in tqdm(recording_list, description, unit=' recordings', smoothing=0):
+        stats_recording = set_default_stat()
         entry_counter = 0
-        recording_type = get_type_of_recording(recording.metadata())
-        if recording_type == RecordingType.ATTACK:
-            recording_type = 'ATTACK'
-        if recording_type == RecordingType.IDLE:
-            recording_type = 'IDLE'
-        if recording_type == RecordingType.NORMAL:
-            recording_type = 'NORMAL'
-        if recording_type == RecordingType.NORMAL_AND_ATTACK:
-            recording_type = 'NORMAL_AND_ATTACK'
+        # calc statistic for every resource in recording
         for resource in recording.resource_stats():
             stats_recording = update_stat(stats_recording,
                                           resource)
             entry_counter += 1
-        for value in ['cpu_usage',
-                      'network_send',
-                      'network_received',
-                      'storage_read',
-                      'storage_written']:
-            stats_recording[value]['avg'] = stats_recording[value]['sum'] / entry_counter
-        tmp_dict[recording_type].append(stats_recording)
-        stats_recording = set_default_stat()
-    return tmp_dict
+        # calc avg value for recording
+        for entry in stats_recording.keys():
+            stats_recording[entry]['avg'] = stats_recording[entry]['sum'] / entry_counter
+        resources_per_recording.append(stats_recording)
+    recording_type_stats = calc_stats_recording(resources_per_recording)
+    return recording_type_stats
 
 
-def calc_avg(counter: int, stats_recording: dict) -> dict:
-    for entry in stats_recording.keys():
-        stats_recording[entry]['avg'] = stats_recording[entry]['sum'] / counter
-    return stats_recording
+def calc_stats_recording(recordings: list) -> dict:
+    """
+
+        receives a list of statistic of recordings
+        and creates statistic of whole list
+
+        Param:
+        recordings (list): list of statistic of recordings
+
+        Returns:
+        dict: statistic of list
 
 
-def full_type_analysis(recording: dict, recording_type_dict: dict) -> dict:
-    for value in ['cpu_usage',
-                  'network_send',
-                  'network_received',
-                  'storage_read',
-                  'storage_written']:
-        if recording_type_dict[value]['min']:
-            if recording[value]['min'] < recording_type_dict['cpu_usage']['min']:
-                recording_type_dict[value]['min'] = recording['cpu_usage']['min']
-        else:
-            recording_type_dict[value]['min'] = recording[value]['min']
-        if recording[value]['max'] > recording_type_dict[value]['max']:
-            recording_type_dict[value]['max'] = recording[value]['max']
-        recording_type_dict[value]['sum'] += recording[value]['avg']
-
-    return recording_type_dict
+    """
+    stats = set_default_stat()
+    for recording in recordings:
+        for value in recording.keys():
+            stats[value] = update_list_entry(recording[value], stats[value])
+    for entry in stats.keys():
+        stats[entry]['avg'] = stats[entry]['sum'] / len(recordings)
+        del stats[entry]['sum']
+    return stats
 
 
-def calc_result_statistic(stats: dict) -> dict:
-    for scenario in stats:
-        result_dict = {
-            'training': {},
-            'validation': {},
-            'test': {},
-        }
-        for category in stats[scenario]:
-            recording_type_dict = set_default_stat()
-            for recording_type in stats[scenario][category]:
-                counter = 0
-                for recording in stats[scenario][category][recording_type]:
-                    counter += 1
-                    recording_type_dict = full_type_analysis(recording, recording_type_dict)
-                for value in ['cpu_usage',
-                              'network_send',
-                              'network_received',
-                              'storage_read',
-                              'storage_written']:
-                    recording_type_dict[value]['avg'] = recording_type_dict[value]['sum'] / counter
-                result_dict[category][recording_type] = recording_type_dict
-        with open(f'Resource_statistics/{scenario}.json', 'w') as file:
-            json.dump(result_dict, file)
-    return result_dict
+def update_list_entry(current_stat: dict, stats: dict) -> dict:
+    """
+
+        udpate min max and sum for a new entry in current stat
+
+        Param:
+        current_stat (dict): new entry to include in statistic
+        stats (dict): statistic keeping track of previously seen entries
+
+        Returns:
+        dict: updated stats dictionary
+
+    """
+    if stats['min'] is None:
+        stats['min'] = current_stat['min']
+    elif current_stat['min'] < stats['min']:
+        stats['min'] = current_stat['min']
+    if current_stat['max'] > stats['max']:
+        stats['max'] = current_stat['max']
+    stats['sum'] = stats['sum'] + current_stat['avg']
+    return stats
 
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description='Statistics for LID-DS 2021 Syscalls')
+
+    parser.add_argument('-d', dest='base_path', action='store', type=str, required=True,
+                        help='LID-DS Base Path')
+    parser.add_argument('-o', dest='output_path', action='store', type=str, required=True,
+                        help='Output Path for statistics')
+
+    args = parser.parse_args()
+
+    # iterate through list of all scenarios
     for scenario in SCENARIO_NAMES:
-        dataloader = DataLoader('../../Dataset/{scenario}')
-        training_data = dataloader.training_data()
-        stats_per_scenario[scenario]['training'] = \
-            stats_per_category(data=training_data,
-                               category='training',
-                               stats_per_category=stats_per_scenario[scenario])
-        validation_data = dataloader.validation_data()
-        stats_per_scenario[scenario]['validation'] = \
-            stats_per_category(data=validation_data,
-                               category='validation',
-                               stats_per_category=stats_per_scenario[scenario])
-        test_data = dataloader.test_data()
-        stats_per_scenario[scenario]['test'] = \
-            stats_per_category(data=test_data,
-                               category='test',
-                               stats_per_category=stats_per_scenario[scenario])
-    result_dict = {}
-    result_dict = calc_result_statistic(stats_per_scenario)
+
+        result_dict = {}
+        scenario_path = os.path.join(args.base_path, scenario)
+        dataloader = DataLoader(scenario_path)
+
+        # dict to describe dataset structure
+        data_parts = {
+            'Training': {
+                'Idle': dataloader.training_data(recording_type=RecordingType.IDLE),
+                'Normal': dataloader.training_data(recording_type=RecordingType.NORMAL)
+            },
+            'Validation': {
+                'Idle': dataloader.validation_data(recording_type=RecordingType.IDLE),
+                'Normal': dataloader.validation_data(recording_type=RecordingType.NORMAL)
+            },
+            'Test': {
+                'Idle': dataloader.test_data(recording_type=RecordingType.IDLE),
+                'Normal': dataloader.test_data(recording_type=RecordingType.NORMAL),
+                'Attack': dataloader.test_data(recording_type=RecordingType.ATTACK),
+                'Normal and Attack': dataloader.test_data(
+                    recording_type=RecordingType.NORMAL_AND_ATTACK)
+            }
+        }
+
+        # calc for Training, Validation and Test data
+        for data_part in data_parts.keys():
+            # calc for Idle, Normal, Attack and Normal and Attack
+            for recording_type in data_parts[data_part].keys():
+                record_result = calc_stats_for_recording_type(data_parts[data_part][recording_type],
+                                                              scenario)
+                if scenario not in result_dict.keys():
+                    result_dict[scenario] = {}
+                if data_part not in result_dict[scenario].keys():
+                    result_dict[scenario][data_part] = {}
+                result_dict[scenario][data_part][recording_type] = record_result
+        save_to_json(result_dict, args.output_path, scenario)
+        break
