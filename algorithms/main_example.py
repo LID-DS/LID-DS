213bd540697e5b334b7d61290fc0d793f3e97d7e
boneymoy88 <38049744+boneymoy88@users.noreply.github.com>
2021-10-20 14:15:51 +0200

    updated syscallIntExtractor (#201)
    * updated syscallIntExtractor
    
    * rename feature extractor
    
    * rename file
    
    * remove file
    
    * Update syscall_to_int.py

--- algorithms/main_example.py
+++ algorithms/main_example.py
@@ -1,6 +1,6 @@
 from algorithms.features.stream_ngram_extractor import StreamNgramExtractor
 from algorithms.features.threadID_extractor import ThreadIDExtractor
-from algorithms.features.syscall_int_extractor import SyscallIntExtractor
+from algorithms.features.syscall_to_int import SyscallToInt
 from algorithms.decision_engines.stide import Stide
 from algorithms.ids import IDS
 from dataloader.data_loader import DataLoader
@@ -11,14 +11,14 @@ if __name__ == '__main__':
     this is an example script to show the usage uf our classes
     """
 
-    syscall_feature_list = [SyscallIntExtractor(),
+    syscall_feature_list = [SyscallToInt(),
                             ThreadIDExtractor()]
-    stream_feature_list = [StreamNgramExtractor(feature_list=[SyscallIntExtractor],
+    stream_feature_list = [StreamNgramExtractor(feature_list=[SyscallToInt],
                                                 thread_aware=True,
                                                 ngram_length=2)]
 
     # data loader for scenario
-    dataloader = DataLoader('/home/grimmer/Work/LID-DS-2021/CVE-2017-7529')
+    dataloader = DataLoader('../../Dataset/CVE-2017-7529/')
 
     dataprocessor = DataPreprocessor(dataloader,
                                      syscall_feature_list,
