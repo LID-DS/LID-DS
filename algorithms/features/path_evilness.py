deb80980af8d1053e3196addaca1fecc84a4bf38
Felix Nirsberger <felix@nirsberger.de>
2021-10-20 15:45:22 +0200

    fixed some final bugs
    added test
    
    documented extractor class

--- algorithms/features/path_evilness.py
+++ algorithms/features/path_evilness.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import typing
 
 from algorithms.features.base_syscall_feature_extractor import BaseSyscallFeatureExtractor
@@ -9,37 +10,83 @@ from treelib.exceptions import DuplicatedNodeIdError
 
 
 class PathEvilness(BaseSyscallFeatureExtractor):
-
-    def __init__(self):
+    def __init__(self, scenario_path, path='Models', force_retrain=False, ):
+        """
+        
+        Feature Extractor that builds a tree for all existing paths in the 
+        training data while training.
+        
+        In the Extraction step the extractor returns 0 for syscalls without 
+        filepath in args and for known paths.
+        
+        If a path is not present in the tree the extractor checks the height 
+        of the anomaly in the tree and returns 1/height resulting in a return
+        value that is always 0 < return_value < 1
+        
+        """
         super().__init__()
+        scenario_name = os.path.basename(os.path.normpath(scenario_path))
+
         self._file_tree = Tree()
         self._file_tree.create_node('root', 'root')
+        self._path = os.path.join(path, f'{scenario_name}-files.tree')
         self._cache = []
+        self._tree_was_loaded = False
+        if not force_retrain:
+            self._load()
 
     def train_on(self, syscall: Syscall):
-        fd = self._get_valid_fd_or_none(syscall.params())
+        """
 
-        if fd is not None:
-            path_list = self._fd_preprocessing(fd)
-            if path_list not in self._cache:
-                self._cache.append(path_list)
-                self._build_file_tree(path_list)
+        takes one systemcall and builds the training buffer
 
-    def _get_valid_fd_or_none(self, params):
-        if 'fd' in params:
-            fd = params['fd']
-            if '<f>' in fd:
-                return fd
-            else:
-                return None
+        """
+        if not self._tree_was_loaded:
+            fd = self._get_valid_fd_or_none(syscall.params())
 
-    def _fd_preprocessing(self, fd):
+            if fd is not None:
+                path_list = self._fd_preprocessing(fd)
+                if path_list not in self._cache:
+                    self._cache.append(path_list)
+                    self._build_file_tree(path_list)
+
+    def _get_valid_fd_or_none(self, params) -> typing.Union[str, None]:
+        """
+
+        checks syscall params for file descriptor tags and extracts its value if present, if not it returns None
+
+        Returns:
+            value of file descriptor param or None
+
+        """
+        param_names = ['fd', 'in_fd', 'out_fd']  # params to investigate
+        for param in param_names:
+            if param in params:
+                fd = params[param]
+                if '<f>' in fd:
+                    return fd
+        return None
+
+    def _fd_preprocessing(self, fd) -> list:
+        """
+
+        preprocesses file descriptor by cutting front and back of the string and splitting it on '/'
+
+        Returns:
+            path as list
+
+        """
         if '<f>' in fd:
-            index = fd.find('<f>')
+            index = fd.find('<f>')  # <f> occurs at the beginning of all real file paths in file descriptors
             clean_path = fd[index + 3:-1]
             return ['root'] + clean_path.split('/')[1:]
 
     def _build_file_tree(self, path_list):
+        """
+
+        builds the file tree on given path
+
+        """
         i = 1
         while i < len(path_list):
             child = path_list[i]
@@ -52,20 +99,53 @@ class PathEvilness(BaseSyscallFeatureExtractor):
             i += 1
 
     def extract(self, syscall: Syscall) -> typing.Tuple[int, object]:
+        """
+
+        calculates evilness by checking if path exists in cache
+        if not it calculates evilness by looking for the height of the first
+        deviating path part
+
+        Returns:
+            evilness between 0 and 1
+
+        """
         evilness = 0
 
         fd = self._get_valid_fd_or_none(syscall.params())
         if fd is not None:
             path_list = self._fd_preprocessing(fd)
             if path_list not in self._cache:
-                for i in range(len(path_list)):
-                    node_id = os.path.join(*path_list[:i+1])
+                i = 1
+                while i < len(path_list):
+                    node_id = os.path.join(*path_list[:i + 1])
+                    parent_id = os.path.join(*path_list[:i])
                     node = self._file_tree.get_node(node_id)
+                    i += 1
                     if node is None:
-                        evilness = 1 / (self._file_tree.depth(node) - 1)
-                        print(evilness)
+                        parent_node = self._file_tree.get_node(parent_id)
+                        evilness = 1 / (self._file_tree.depth(parent_node) + 1)
                         break
 
-
-
         return PathEvilness.get_id(), evilness
+
+    def fit(self):
+        """
+
+        persists tree and finalizes training
+
+        """
+        file_handler = open(self._path, 'wb')
+        pickle.dump(self._file_tree, file_handler)
+
+    def _load(self):
+        """
+
+        loads tree from models folder if it already exists
+
+        """
+        try:
+            file_handler = open(self._path, 'rb')
+            self._file_tree = pickle.load(file_handler)
+            self._tree_was_loaded = True
+        except FileNotFoundError:
+            pass
---
+++ algorithms/features/test/test_path_evilness.py
@@ -0,0 +1,37 @@
+from algorithms.features.path_evilness import PathEvilness
+from dataloader.syscall import Syscall
+
+
+def test_path_evilness():
+    syscall_1 = Syscall(
+        "1631209047761484608 0 3686302 apache2 3686302 open < fd=9(<f>/proc/sys/kernel/ngroups_max) name=/proc/sys/kernel/ngroups_max flags=1(O_RDONLY) mode=0 dev=200024")
+
+    syscall_2 = Syscall(
+        "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>/proc/sys/kernel/ngroups_min) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    syscall_3 = Syscall(
+        "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>/etc/group) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    syscall_4 = Syscall(
+        "1631209047762064269 0 3686303 apache2 3686303 open < in_fd=9(<f>/etc/test) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    syscall_5 = Syscall(
+        "1631209047762064269 0 3686303 apache2 3686303 open < out_fd=9(<f>/etc/password) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    syscall_6 = Syscall(
+        "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>/proc/sys/kernel/evil) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    training_syscalls = [syscall_1, syscall_2, syscall_3, syscall_4]
+    extractor = PathEvilness(scenario_path='/Test/test', path='algorithms/Models')
+
+    for syscall in training_syscalls:
+        extractor.train_on(syscall)
+
+    extractor.fit()
+
+    evilness = extractor.extract(syscall_5)
+    assert evilness == (PathEvilness.get_id(), 0.5)
+
+    evilness = extractor.extract(syscall_6)
+    assert evilness == (PathEvilness.get_id(), 0.25)
+
