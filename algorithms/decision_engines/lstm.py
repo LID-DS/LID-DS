20c2d09d0c834e18e96c82846a249f5bbd5f9e32
tk <tim_k@posteo.de>
2021-10-26 15:31:51 +0200

    Now with custom batch creation, aware of new recordings.

--- algorithms/decision_engines/lstm.py
+++ algorithms/decision_engines/lstm.py
@@ -11,24 +11,48 @@ from algorithms.decision_engines.base_decision_engine import BaseDecisionEngine
 
 
 class LSTM(BaseDecisionEngine):
+    """
+
+    LSTM decision engine
+
+    Main idea:
+        training:
+            train prediction of next systemcall given a feature vector
+        prediction:
+            convert logits return value of model for every syscall to possibilties with softmax
+            check predicted possibility of actual syscall and return 1-pred_pos as anomaly score
+
+
+    """
 
     def __init__(self,
-                 ngram_length,
-                 embedding_size,
-                 distinct_syscalls,
+                 ngram_length: int,
+                 embedding_size: int,
+                 distinct_syscalls: int,
                  extra_param=0,
                  epochs=300,
-                 streaming_window_size=1,
                  architecture=None,
-                 predict_on_batch=False,
                  batch_size=1,
                  model_path='Models/',
                  force_train=False):
+        """
+
+        Args:
+            ngram_length:       count of embedded syscalls
+            embedding_size:     size of one embedded syscall
+            distinct_syscalls:  amount of distinct syscalls in training data
+            extra_param:        amount of used extra parameters
+            epochs:             set training epochs of LSTM
+            architecture:       type of LSTM architecture
+            batch_size:         set maximum batch_size
+            model_path:         path to save trained Net to
+            force_train:        force training of Net
+
+        """
         self._ngram_length = ngram_length
         self._embedding_size = embedding_size
         self._extra_param = extra_param
         self._batch_size = batch_size
-        self._predict_on_batch = predict_on_batch
         self._epochs = epochs
         self._distinct_syscalls = distinct_syscalls
         self._model_path = model_path \
@@ -37,8 +61,12 @@ class LSTM(BaseDecisionEngine):
             'x': [],
             'y': []
         }
+        self._state = 'build_training_data'
         self._architecture = architecture
         self._lstm = None
+        self._batch_indices = []
+        self._current_batch = []
+        self._batch_counter = 0
         self._hidden_state = None
         self._cell_state = None
         self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -51,6 +79,11 @@ class LSTM(BaseDecisionEngine):
                 print(f"Did not load Model {self._model_path}.")
 
     def _set_model(self, distinct_syscalls: int, device: str):
+        """
+
+        create LSTM Net with outputlayer of distinct_syscalls + 1 (one extra for unknown syscalls)
+
+        """
         input_dim = self._ngram_length * (self._extra_param + self._embedding_size)
         hidden_dim = 64
         n_layers = 1
@@ -63,38 +96,69 @@ class LSTM(BaseDecisionEngine):
                          batch_size=self._batch_size)
 
     def train_on(self, feature_list: list):
+        """
+
+        create training data and keep track of batch indices
+        batch indices are later used for creation of batches
+
+        Args:
+            feature_list (int): list of prepared features for DE
+
+        """
         if self._lstm is None:
             x = np.array(feature_list[1:])
             y = feature_list[0][0]
             self._training_data['x'].append(x)
             self._training_data['y'].append(y)
+            self._current_batch.append(self._batch_counter)
+            self._batch_counter += 1
+            if len(self._current_batch) == self._batch_size:
+                self._batch_indices.append(self._current_batch)
+                self._current_batch = []
         else:
             pass
 
     def fit(self):
+        """
+
+        fit model only if it could not be loaded
+        set model state.
+        convert training data to tensors and feed into custom dataset
+        call torch dataloader with prepared batch_indices
+            needed because end of recording cuts a batch
+        create actual net for fitting
+        define hyperparameters, iterate through DataSet and train Net
+        keep hidden and cell state over batches, only reset with new recording
+
+
+        """
+        if self._state == 'build_training_data':
+            self._state = 'fitting'
         if self._lstm is None:
             x_tensors = Variable(torch.Tensor(self._training_data['x'])).to(self._device)
             y_tensors = Variable(torch.Tensor(self._training_data['y'])).to(self._device)
             y_tensors = y_tensors.long()
             x_tensors_final = torch.reshape(x_tensors, (x_tensors.shape[0], 1, x_tensors.shape[1]))
             dataset = SyscallFeatureDataSet(x_tensors_final, y_tensors)
-            dataloader = DataLoader(dataset, batch_size=self._batch_size)
+            # for custom batches
+            dataloader = DataLoader(dataset, batch_sampler=self._batch_indices)  # batch_size=self._batch_size)
             print(f"Training Shape x: {x_tensors_final.shape} y: {y_tensors.shape}")
             self._set_model(self._distinct_syscalls, self._device)
             self._lstm.to(self._device)
+            preds = []
+            # Net hyperparameters
             learning_rate = 0.001
             criterion = torch.nn.CrossEntropyLoss()
             optimizer = torch.optim.Adam(self._lstm.parameters(), lr=learning_rate)
+            self._hidden_state = None
+            self._cell_state = None
             torch.manual_seed(1)
-            preds = []
-            hidden_state = None
-            cell_state = None
             for epoch in tqdm(range(self._epochs), 'training network:'.rjust(25), unit=" epochs"):
                 for i, data in enumerate(dataloader, 0):
                     inputs, labels = data
-                    outputs, hidden_state, cell_state = self._lstm.forward(inputs,
-                                                                           hidden_state,
-                                                                           cell_state)
+                    outputs, self._hidden_state, self_cell_state = self._lstm.forward(inputs,
+                                                                                      self._hidden_state,
+                                                                                      self._cell_state)
                     # caluclate the gradient, manually setting to 0
                     optimizer.zero_grad()
                     # obtain the loss function
@@ -109,28 +173,38 @@ class LSTM(BaseDecisionEngine):
                 preds = []
                 print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()))
                 # reset hidden state
-                hidden_state = None
-                cell_state = None
+                self.new_recording()
             torch.save(self._lstm.state_dict(), self._model_path)
         else:
             print(f"Net already trained. Using model {self._model_path}")
             pass
 
-    def predict(self, feature_list: list, hidden_state, cell_state) -> float:
-        # self._lstm.init_hidden_state()
+    def predict(self, feature_list: list) -> float:
+        """
+
+        remove label from feature_list and feed into model.
+        calc possibility of prediction_probs[0][actual_call] and return
+        1 - calculated possibility as anomaly score
+
+        """
         x_tensor = Variable(torch.Tensor(np.array([feature_list[1:]])))
         x_tensor_final = torch.reshape(x_tensor, (x_tensor.shape[0], 1, x_tensor.shape[1]))
         actual_syscall = feature_list[0][0]
         prediction_logits, hidden_state, cell_state = self._lstm(x_tensor_final,
-                                                                 hidden_state,
-                                                                 cell_state)
+                                                                 self._hidden_state,
+                                                                 self._cell_state)
         softmax = nn.Softmax()
         prediction_probs = softmax(prediction_logits)
         predicted_probability = prediction_probs[0][actual_syscall]
         anomaly_score = 1 - predicted_probability
-        return anomaly_score, hidden_state, cell_state
+        return anomaly_score
 
     def _accuracy(self, outputs, labels):
+        """
+
+        calculate accuracy of last epoch
+
+        """
         hit = 0
         miss = 0
         for i in range(len(outputs) - 1):
@@ -141,7 +215,23 @@ class LSTM(BaseDecisionEngine):
         print(f"accuracy {hit/(hit+miss)}")
 
     def new_recording(self):
-        self._lstm.init_states(self._batch_size)
+        """
+
+        while creation of dataset:
+            cut batch after recording end
+        while fitting and detecting
+            reset hidden state
+
+        """
+        if self._lstm is None and self._state == 'build_training_data':
+            if len(self._current_batch) > 0:
+                self._batch_indices.append(self._current_batch)
+                self._current_batch = []
+        elif self._state == 'fitting':
+            self._hidden_state = None
+            self._cell_state = None
+        else:
+            pass
 
 
 class Net(nn.Module):
@@ -179,7 +269,9 @@ class Net(nn.Module):
         else:
             self._hidden_state = hidden_state
             self._cell_state = cell_state
-        output, (new_hidden_state, new_cell_state) = self.lstm(x, (self._hidden_state, self._cell_state))
+        output, (new_hidden_state, new_cell_state) = self.lstm(x,
+                                                               (self._hidden_state,
+                                                                self._cell_state))
         # internal state
         # reshaping the data for Dense layer next
         new_hidden_state = new_hidden_state.view(-1, self.hidden_size)
@@ -189,9 +281,11 @@ class Net(nn.Module):
 
     def init_states(self, batch_size: int):
         # hidden state
-        self._hidden_state = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)).to(self._device)
+        self._hidden_state = Variable(
+            torch.zeros(self.num_layers, batch_size, self.hidden_size)).to(self._device)
         # internal state
-        self._cell_state = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)).to(self._device)
+        self._cell_state = Variable(
+            torch.zeros(self.num_layers, batch_size, self.hidden_size)).to(self._device)
 
 
 class SyscallFeatureDataSet(Dataset):
@@ -209,3 +303,14 @@ class SyscallFeatureDataSet(Dataset):
         _x = self.X[index]
         _y = self.Y[index]
         return _x, _y
+
+
+class EndBatchWithRecording:
+    """
+
+    Custom Batch Sampler to end batch with 'new_recording' marker
+
+    """
+
+    def __init__(self, batch_sampler):
+        pass
