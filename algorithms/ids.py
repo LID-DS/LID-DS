3b9beec359f6f309afed70914bff3d795e2bf88c
Mlyfall <ephcs@web.de>
2021-11-01 10:50:25 +0100

    new performance values: tp/fn and detection rate on file level, using unix time in seconds #205

--- algorithms/ids.py
+++ algorithms/ids.py
@@ -1,11 +1,8 @@
-from typing import Union, Type, Generator
-
 from tqdm import tqdm
 
 from algorithms.decision_engines.base_decision_engine import BaseDecisionEngine
 from dataloader.base_data_loader import BaseDataLoader
 from dataloader.data_preprocessor import DataPreprocessor
-from dataloader.syscall import Syscall
 
 
 class IDS:
@@ -19,6 +16,10 @@ class IDS:
         self._threshold = 0.0
         self._performance_values = {}
         self._alarm = False
+        self._anomaly_scores_exploits = []
+        self._anomaly_scores_no_exploits = []
+        self._first_syscall_after_exploit_list = []
+        self._last_syscall_of_recording_list = []
 
     def train_decision_engine(self):
         # train of DE
@@ -50,11 +51,8 @@ class IDS:
 
     def do_detection(self):
         """
-            detects: false positives, true positives, true negatives, false negatives, consecutive false alarms
-                         from feature_vectors and metadata
-
-            returns: counts of fp, tp, tn, fn, cfa as int, alarms per rec
-
+            detects: false positives, true positives:alarms, true negatives, false negatives, consecutive false alarms,
+                     detection rate based on anomaly scores and metadata
         """
         fp = 0
         tp = 0
@@ -67,71 +65,115 @@ class IDS:
         data = self._data_loader.test_data()
         description = 'anomaly detection: '
 
+        # syscall index and exploit count needed for plotting
+        syscall_count_for_plot_exploit_recordings = 1
+        exploit_count = 0
+
         for recording in tqdm(data, description, unit=" recording"):
             if self._alarm is not False:
                 self._alarm = False
+
             if recording.metadata()["exploit"] is True:
+                exploit_exists = True
                 exploit_time = recording.metadata()["time"]["exploit"][0]["absolute"]
+                exploit_count += 1
             else:
+                exploit_exists = False
                 exploit_time = None
 
-            for syscall in recording.syscalls():
+            first_sys_after_exploit = False
 
-                syscall_time = Syscall.timestamp_unix_in_ns(syscall) * (10 ** (-9))
+            for syscall in recording.syscalls():
+                syscall_time = syscall.timestamp_unix_in_ns() * (10 ** (-9))
                 feature_vector = self._data_preprocessor.syscall_to_feature(syscall)
+
+                # getting index of first syscall after exploit of each recording for plotting
+                if exploit_exists is True and syscall_time >= exploit_time and first_sys_after_exploit is False:
+                    self._first_syscall_after_exploit_list.append(syscall_count_for_plot_exploit_recordings)
+                    first_sys_after_exploit = True
+
+                if exploit_time is not None:
+                    syscall_count_for_plot_exploit_recordings += 1
 
                 if feature_vector is not None:
                     anomaly_score = self._decision_engine.predict(feature_vector)
 
-                    if anomaly_score > self._threshold:
-                        if exploit_time is not None:
+                    # saving scores separately for plotting
+                    if exploit_time is not None:
+                        self._anomaly_scores_exploits.append(anomaly_score)
+                    if exploit_time is None:
+                        self._anomaly_scores_no_exploits.append(anomaly_score)
+
+                    # files with exploit
+                    if exploit_time is not None:
+                        if anomaly_score > self._threshold:
                             if exploit_time > syscall_time:
                                 fp += 1
                                 cfa_stream += 1
-                            elif exploit_time < syscall_time and self._alarm is False:
-                                tp += 1
-                                alarm_count += 1
-                                self._alarm = True
-                            elif exploit_time < syscall_time and self._alarm is True:
-                                tp += 1
-                        else:
-                            fp += 1
+                            elif exploit_time < syscall_time:
+                                if self._alarm is False:
+                                    tp += 1
+                                    alarm_count += 1
+                                    self._alarm = True
+                                elif self._alarm is True:
+                                    tp += 1
 
-                    if anomaly_score < self._threshold:
-                        if exploit_time is not None:
+                        elif anomaly_score < self._threshold:
                             if cfa_stream > 0:
                                 cfa_stream = 0
                                 cfa_count += 1
-
                             if exploit_time > syscall_time:
                                 tn += 1
                             elif exploit_time < syscall_time:
                                 fn += 1
-                            else:
-                                tn += 1
+
+                    # files without exploit
+                    elif exploit_time is None:
+                        if anomaly_score > self._threshold:
+                            fp += 1
+                            cfa_stream += 1
+                        if anomaly_score < self._threshold:
+                            if cfa_stream > 0:
+                                cfa_stream = 0
+                                cfa_count += 1
+                            tn += 1
+
+            # getting index of last syscall of each recording for plotting
+            if exploit_time is not None:
+                self._last_syscall_of_recording_list.append(syscall_count_for_plot_exploit_recordings)
+
             self._data_preprocessor.new_recording()
             self._decision_engine.new_recording()
 
-        re = tp / (tp + fn)
-        pr = tp / (tp + fp)
+        try:
+            re = alarm_count / exploit_count
+        except ZeroDivisionError:
+            print("Division by Zero not possible, no exploits counted.")
 
         self._performance_values = {"false positives": fp,
                                     "true positives": tp,
                                     "true negatives": tn,
                                     "false negatives": fn,
-                                    "alarms in recording": alarm_count,
+                                    "recording with detected alarm count/true positives on file level": alarm_count,
+                                    "exploit count": exploit_count,
+                                    "false negatives on file level": exploit_count - alarm_count,
+                                    "detection rate": alarm_count / exploit_count,
                                     "consecutive false alarms": cfa_count,
-                                    "Recall": re,
-                                    "Precision": pr,
-                                    "F1": 2 * ((pr * re) / (pr + re))}
-
-
+                                    "recall file level": re,
+                                    }
 
     def get_performance(self):
 
         """
         returns dict with performance values
-
         """
 
         return self._performance_values
+
+    def get_plotting_data(self):
+
+        """
+           returns relevant information for plot
+        """
+
+        return self._threshold, self._first_syscall_after_exploit_list, self._last_syscall_of_recording_list, self._anomaly_scores_exploits, self._anomaly_scores_no_exploits
