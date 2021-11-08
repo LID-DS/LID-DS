b79401224760f175b437d82e772dddfed5613944
tk <tim_k@posteo.de>
2021-11-08 14:46:58 +0100

    implement new feature thread_change_flag which needed change in data_processor

--- algorithms/ids_lstm_main.py
+++ algorithms/ids_lstm_main.py
@@ -1,15 +1,13 @@
 from algorithms.features.ngram_plus_next_syscall import NgramPlusNextSyscall
-from algorithms.features.ngram_minus_one import NgramMinusOne
 from algorithms.features.threadID_extractor import ThreadIDExtractor
 # from algorithms.features.time_delta_syscalls import TimeDeltaSyscalls
-# from algorithms.features.thread_change_flag import ThreadChangeFlag
+from algorithms.features.thread_change_flag import ThreadChangeFlag
 from algorithms.features.syscall_to_int import SyscallToInt
 from algorithms.features.w2v_embedding import W2VEmbedding
 from dataloader.data_preprocessor import DataPreprocessor
-from dataloader.data_loader_2019 import DataLoader
+from dataloader.data_loader import DataLoader
 from algorithms.decision_engines.lstm import LSTM
 from algorithms.ids import IDS
-from score_plot import ScorePlot
 
 import pprint
 
@@ -17,11 +15,10 @@ if __name__ == '__main__':
     """
     this is an example script to show the usage uf our classes
     """
-    ngram_length = 3
+    ngram_length = 2
     embedding_size = 4
     thread_aware = True
-    scenario = "CVE-2014-0160"
-    scenario_path = f'../../Dataset_old/{scenario}/'
+    scenario_path = '../../Dataset/CVE-2017-7529/'
     syscall_feature_list = [W2VEmbedding(vector_size=embedding_size,
                                          window_size=ngram_length,
                                          epochs=100,
@@ -32,24 +29,26 @@ if __name__ == '__main__':
     stream_feature_list = [NgramPlusNextSyscall(feature_list=[W2VEmbedding],
                                                 thread_aware=thread_aware,
                                                 ngram_length=ngram_length)]
+    feature_of_stream_feature_list = [ThreadChangeFlag(syscall_feature_list=[ThreadIDExtractor],
+                                                       stream_feature_list=[NgramPlusNextSyscall])]
 
     # data loader for scenario
     dataloader = DataLoader(scenario_path)
 
     dataprocessor = DataPreprocessor(dataloader,
                                      syscall_feature_list,
-                                     stream_feature_list)
+                                     stream_feature_list,
+                                     feature_of_stream_feature_list)
     # decision engine (DE)
     distinct_syscalls = dataloader.distinct_syscalls_training_data()
     lstm = LSTM(ngram_length=ngram_length,
                 embedding_size=embedding_size,
                 distinct_syscalls=distinct_syscalls,
                 epochs=20,
-                batch_size=256,
-                force_train=False,
-                model_path=f'Models/{scenario}/')
+                batch_size=128,
+                force_train=True,
                 # time_delta=0,
-                # thread_change_flag=0,
+                thread_change_flag=1)
                 # return_value=0)
 
     # define the used features
@@ -61,9 +60,3 @@ if __name__ == '__main__':
     ids.determine_threshold()
     ids.do_detection()
     pprint.pprint(ids.get_performance())
-
-    # creating plot
-    plot = ScorePlot(scenario_path=dataloader.scenario_path)
-
-    plot.feed_figure(ids.get_plotting_data())
-    plot.show_plot()
