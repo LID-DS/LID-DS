face2b9eda82631638322ccb5576da4c1065f267
Mlyfall <ephcs@web.de>
2021-11-09 11:24:42 +0100

    plot switch #240

--- algorithms/ids.py
+++ algorithms/ids.py
@@ -16,7 +16,7 @@ class IDS:
         self._data_loader = data_loader
         self._data_preprocessor = data_preprocessor
         self._decision_engine = decision_engine
-        self.plot_switch = plot
+        self._plot_switch = plot
         self.threshold = 0.0
         self._alarm = False
         self._anomaly_scores_exploits = []
@@ -24,8 +24,8 @@ class IDS:
         self._first_syscall_after_exploit_list = []
         self._last_syscall_of_recording_list = []
         self.performance = PerformanceMeasurement()
-        if self.plot_switch is True:
-            self.plot = ScorePlot(data_loader.scenario_path, self.plot_switch)
+        if self._plot_switch is True:
+            self.plot = ScorePlot(data_loader.scenario_path, self._plot_switch)
 
     def train_decision_engine(self):
         # train of DE
