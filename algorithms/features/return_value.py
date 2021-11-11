dec0732aa0fcffe62311b1c39acca2f6b85dca06
tk <tim_k@posteo.de>
2021-11-11 16:07:36 +0100

    actually pretty shitty code

--- algorithms/features/return_value.py
+++ algorithms/features/return_value.py
@@ -11,6 +11,8 @@ class ReturnValue(BaseSyscallFeatureExtractor):
         self._max_time_delta = 0
         self._last_time = {}
         self.type_dict = {}
+        self._max_returned_bytes_read = 0
+        self._max_returned_bytes_write = 0
 
     def train_on(self, syscall: Syscall):
         """
@@ -18,24 +20,66 @@ class ReturnValue(BaseSyscallFeatureExtractor):
         calc max time delta
 
         """
-        param = syscall.param('res')
-        print(param)
-        if param:
-            self.type_dict[type(param)] = 1
+        return_value_string = syscall.param('res')
+        if return_value_string is not None:
+            try:
+                current_bytes = int(return_value_string)
+                if current_bytes > 1 and current_bytes < 1000000000:
+                    if "write" in return_value_string:
+                        if current_bytes >= self._max_returned_bytes_write:
+                            self._max_returned_bytes_write = current_bytes
+                    elif "sendfile" in return_value_string:
+                        if current_bytes >= self._max_returned_bytes_write:
+                            self._max_returned_bytes_write = current_bytes
+                    elif "read" in return_value_string:
+                        if current_bytes >= self._max_returned_bytes_read:
+                            self._max_returned_bytes_read = current_bytes
+                    elif "recv" in return_value_string:
+                        # recv msg from socket
+                        pass
+                    else:
+                        # print("else")
+                        print(self._max_returned_bytes_read)
+                        print(self._max_returned_bytes_write)
+                        print(syscall.name(), return_value_string)
+                        b = 'write' in return_value_string
+                        # print(b)
+            except Exception:
+                pass
+                #print(syscall.name(), return_value_string)
         # delta = self._calc_delta(current_time, syscall)
         # if delta > self._max_time_delta:
         # self._max_time_delta = delta
 
     def fit(self):
-        print(self.type_dict)
+        print(self._max_returned_bytes_read)
+        print(self._max_returned_bytes_write)
 
     def extract(self, syscall: Syscall) -> typing.Tuple[int, float]:
         """
-
-        extract thread ID of syscall
-
         """
-        current_time = syscall.timestamp_datetime()
-        delta = self._calc_delta(current_time, syscall)
-        normalized_delta = delta / self._max_time_delta
-        return ReturnValue.get_id(), normalized_delta
+        return_value_string = syscall.param('res')
+        return_type = 'read'
+        if return_value_string is not None:
+            try:
+                current_bytes = int(return_value_string)
+                if current_bytes > 1 and current_bytes < 1000000000:
+                    if "write" in return_value_string:
+                        return_type = 'write'
+                    elif "sendfile" in return_value_string:
+                        return_type = 'write'
+                    else:
+                        # print("else")
+                        print(max_returned_bytes_read)
+                        print(max_returned_bytes_write)
+                        print(syscall.name(), return_value_string)
+                        b = 'write' in return_value_string
+                        # print(b)
+            except Exception:
+                pass
+            if return_type == 'read':
+                normalized_bytes = current_bytes/self._max_retruned_bytes_read
+            else:
+                normalized_bytes = current_bytes/self._max_retruned_bytes_write
+            return normalized_bytes
+
