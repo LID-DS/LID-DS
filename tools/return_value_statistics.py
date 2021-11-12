41f2556c9c67513f20a2292a64458f878128996b
tk <tim_k@posteo.de>
2021-11-12 13:15:00 +0100

    now statistic average of syscalls not files

--- tools/return_value_statistics.py
+++ tools/return_value_statistics.py
@@ -26,20 +26,34 @@ def calc_return_value_stats(recording_list, description):
     exploit_count = 0
     result_dict = {
         'normal': {
-            'bytes_written' : 0,
-            'bytes_read' : 0,
-            'bytes_socket' : 0,
-            'bytes_kernel' : 0
+            'bytes_written': 0,
+            'bytes_read': 0,
+            'bytes_socket': 0,
+            'bytes_kernel': 0
         },
         'exploit': {
-            'bytes_written' : 0,
-            'bytes_read' : 0,
-            'bytes_socket' : 0,
-            'bytes_kernel' : 0
+            'bytes_written': 0,
+            'bytes_read': 0,
+            'bytes_socket': 0,
+            'bytes_kernel': 0
         }
     }
     read = ['read', 'getdents']
     for recording in tqdm(recording_list, description, unit=" recordings", smoothing=0):
+        rec_stats = {
+            'counter': {
+                'written': 0,
+                'read': 0,
+                'socket': 0,
+                'kernel': 0
+            },
+            'bytes': {
+                'written': 0,
+                'read': 0,
+                'kernel': 0,
+                'socket': 0
+            }
+        }
         if recording.recording_data_list[RecordingDataParts.IS_EXECUTING_EXPLOIT] == 'True':
             exploit_count += 1
             rec = 'exploit'
@@ -53,38 +67,69 @@ def calc_return_value_stats(recording_list, description):
                     return_value_int = int(return_value_string)
                     if return_value_int > 10 and return_value_int < 10000000000:
                         if 'write' in syscall.name():
-                            result_dict[rec]['bytes_written'] += return_value_int
+                            rec_stats['bytes']['written'] += return_value_int
+                            rec_stats['counter']['written'] += 1
                         elif any(string in syscall.name() for string in read):
-                            result_dict[rec]['bytes_read'] += return_value_int
+                            rec_stats['bytes']['read'] += return_value_int
+                            rec_stats['counter']['read'] += 1
                         elif 'send' in syscall.name():
-                            result_dict[rec]['bytes_kernel'] += return_value_int
+                            rec_stats['bytes']['kernel'] += return_value_int
+                            rec_stats['counter']['kernel'] += 1
                         elif 'recv' in syscall.name():
-                            result_dict[rec]['bytes_socket'] += return_value_int
+                            rec_stats['bytes']['socket'] += return_value_int
+                            rec_stats['counter']['socket'] += 1
                         elif 'clone' in syscall.name():
                             # returns threadID
+                            continue
+                        elif 'futex' in syscall.name():
+                            # not returning bytes
                             continue
                         elif 'lseek' in syscall.name():
                             # returns file descriptor offset
                             continue
-                        else:
+                        elif 'fcntl' in syscall.name():
+                            # returns flags or file descriptor
                             continue
-                            # print(syscall.name(), return_value_int)
+                        elif 'getcwd' in syscall.name():
+                            # returns pointer to directory string
+                            continue
+                        elif 'brk' in syscall.name():
+                            # returns success or errno
+                            continue
+                        else:
+                            print(syscall.name(), return_value_int)
                 except Exception:
                     pass
+        try:
+            result_dict[rec]['bytes_read'] += rec_stats['bytes']['read'] / rec_stats['counter']['read']
+        except ZeroDivisionError:
+            result_dict[rec]['bytes_read'] += 0
+        try:
+            result_dict[rec]['bytes_written'] += rec_stats['bytes']['written'] / rec_stats['counter']['written']
+        except ZeroDivisionError:
+            result_dict[rec]['bytes_written'] += 0
+        try:
+            result_dict[rec]['bytes_kernel'] += rec_stats['bytes']['kernel'] / rec_stats['counter']['kernel']
+        except ZeroDivisionError:
+            result_dict[rec]['bytes_kernel'] += 0
+        try:
+            result_dict[rec]['bytes_socket'] += rec_stats['bytes']['socket'] / rec_stats['counter']['socket']
+        except ZeroDivisionError:
+            result_dict[rec]['bytes_socket'] += 0
     if exploit_count == 0:
         exploit_count = 1
     result_dict = {
         'normal': {
-            'bytes_written' : int(result_dict['normal']['bytes_written']/normal_count),
-            'bytes_read' : int(result_dict['normal']['bytes_read']/normal_count),
-            'bytes_socket' : int(result_dict['normal']['bytes_socket']/normal_count),
-            'bytes_kernel' : int(result_dict['normal']['bytes_kernel']/normal_count)
+            'bytes_written': int(result_dict['normal']['bytes_written']/normal_count),
+            'bytes_read': int(result_dict['normal']['bytes_read']/normal_count),
+            'bytes_socket': int(result_dict['normal']['bytes_socket']/normal_count),
+            'bytes_kernel': int(result_dict['normal']['bytes_kernel']/normal_count)
         },
         'exploit': {
-            'bytes_written' : int(result_dict['exploit']['bytes_written']/exploit_count),
-            'bytes_read' : int(result_dict['exploit']['bytes_read']/exploit_count),
-            'bytes_socket' : int(result_dict['exploit']['bytes_socket']/exploit_count),
-            'bytes_kernel' : int(result_dict['exploit']['bytes_kernel']/exploit_count)
+            'bytes_written': int(result_dict['exploit']['bytes_written']/exploit_count),
+            'bytes_read': int(result_dict['exploit']['bytes_read']/exploit_count),
+            'bytes_socket': int(result_dict['exploit']['bytes_socket']/exploit_count),
+            'bytes_kernel': int(result_dict['exploit']['bytes_kernel']/exploit_count)
         }
     }
     return result_dict
@@ -93,13 +138,15 @@ def calc_return_value_stats(recording_list, description):
 if __name__ == '__main__':
 
     SCENARIO_NAMES = [
-        "Bruteforce_CWE-307",
-        "CVE-2012-2122",
-        "CVE-2014-0160",
-        "CVE-2017-7529",
-        "CVE-2018-3760",
-        "CVE-2019-5418",
-        "PHP_CWE-434",
+        # "Bruteforce_CWE-307",
+        # "CVE-2012-2122",
+        # "CVE-2014-0160",
+        # "CVE-2017-7529",
+        # "CVE-2018-3760",
+        # "CVE-2019-5418",
+        # "PHP_CWE-434",
+        "EPS_CWE-434",
+        "ZipSlip"
     ]
     # iterates through list of all scenarios, main loop
     for scenario in SCENARIO_NAMES:
