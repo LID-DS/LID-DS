9eea4abf3c25db913066c2059624fb6513f1dcfb
tk <tim_k@posteo.de>
2021-11-22 14:31:31 +0100

    clean pull-request

--- algorithms/features/thread_change_flag.py
+++ algorithms/features/thread_change_flag.py
@@ -1,69 +1,86 @@
 import typing
 from collections import deque
-from collections.abc import Iterable
 
 from algorithms.features.threadID_extractor import ThreadIDExtractor
+from algorithms.features.syscall_to_int import SyscallToInt
 from algorithms.features.base_stream_feature_extractor import BaseStreamFeatureExtractor
 
 
 class ThreadChangeFlag(BaseStreamFeatureExtractor):
     """
-    receives ngram stream feature and threadID feature
+    extract ngram form a stream of system call features
+    include n+1 system call as integer for supervised learning
+    also add thread change flag information
     set thread change flag to 1 if ngram predecessor is from different thread
     othrewise 0
-    initially also set to 0
-    return received features plus appended thread change flag
     """
 
-    def __init__(self, syscall_feature_list: list, stream_feature_list: list):
+    def __init__(self, feature_list: list, thread_aware: bool, ngram_length: int):
         """
         """
         super().__init__()
+        self._ngram_buffer = {}
         self._list_of_feature_ids = []
-        for feature_class in stream_feature_list:
+        for feature_class in feature_list:
+            # skip systoint which is needed for y value in list
+            # y is label for syscall to predict
+            # if feature_class.get_id() == SyscallToInt.get_id():
+            # continue
             self._list_of_feature_ids.append(feature_class.get_id())
-        self._last_thread_id = None
-        self._syscall_feature_list = syscall_feature_list
-        if ThreadIDExtractor not in syscall_feature_list:
-            raise KeyError('No thread id in features')
+        self._thread_aware = thread_aware
+        self._ngram_length = ngram_length
+        self._last_thread_id = 0
 
-    def extract(self, syscall_features: dict, stream_features: dict) -> typing.Tuple[str, list]:
+    def extract(self, syscall_features: dict) -> typing.Tuple[str, list]:
         """
-        check if stream features have been provided otherwise return None
-        get current threadID of syscall
-        compare with last thread id and determine thread change flag accordingly
-        collect stream features with passed features and append thread change flag
+        only returns not None if ngram exists
         """
-        if len(stream_features) == 0:
-            return ThreadChangeFlag.get_id(), None
-        try:
-            thread_id = syscall_features[ThreadIDExtractor.get_id()]
-        except KeyError:
-            raise KeyError('No thread id in features')
-        if self._last_thread_id is None:
-            self._last_thread_id = thread_id
-            thread_change_flag = 0
-        elif thread_id == self._last_thread_id:
-            thread_change_flag = 0
-        else:
-            thread_change_flag = 1
-            self._last_thread_id = thread_id
-        features = self._collect_features(stream_features)
-        features.append(thread_change_flag)
-        return ThreadChangeFlag.get_id(), features
+        # get current threadID of syscall
+        thread_id = 0
+        if self._thread_aware:
+            try:
+                thread_id = syscall_features[ThreadIDExtractor.get_id()]
+            except Exception:
+                raise KeyError('No thread id in features')
+        # if current buffer for thread is full append syscall integer
+        # to ngram_value which later can be used as x, y for DE input
+        ngram_value = None
+        if thread_id in self._ngram_buffer:
+            if len(self._ngram_buffer[thread_id]) == self._ngram_length:
+                if thread_id != self._last_thread_id:
+                    thread_change_flag = 1
+                    self._last_thread_id = thread_id
+                else:
+                    thread_change_flag = 0
+                ngram_value = self._collect_features(self._ngram_buffer[thread_id])
+                # print(ngram_value)
+                ngram_value = \
+                    syscall_features[SyscallToInt.get_id()] \
+                    + ngram_value \
+                    + [thread_change_flag]
+                if thread_change_flag == 1:
+                    print(ngram_value)
+                # print(ngram_value)
+        # but also add syscall to buffer
+        if thread_id not in self._ngram_buffer:
+            self._ngram_buffer[thread_id] = deque(maxlen=self._ngram_length)
+        self._ngram_buffer[thread_id].append(syscall_features)
+        return ThreadChangeFlag.get_id(), ngram_value
 
-    def _collect_features(self, stream_feature_dict: deque) -> list:
+    def _collect_features(self, deque_of_dicts: deque) -> list:
         """
-        creates list of features included in stream_feature_list
-
-        Returns:
-            list: concatenated values of stream_feature_dict
+        creates list of deque of features included in feature_list
         """
         array = []
-        for feature_id in self._list_of_feature_ids:
-            if feature_id in stream_feature_dict:
-                if isinstance(stream_feature_dict[feature_id], Iterable):
-                    array += stream_feature_dict[feature_id]
-                else:
-                    array.append(stream_feature_dict[feature_id])
+        for feature_dict in deque_of_dicts:
+            for feature_id in self._list_of_feature_ids:
+                if feature_id in feature_dict:
+                    array.extend(feature_dict[feature_id])
         return array
+
+    def new_recording(self):
+        """
+        empty buffer so ngrams consist of same recording only
+        """
+        self._ngram_buffer = {}
+        self._last_thread_id = 0
