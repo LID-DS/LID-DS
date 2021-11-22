d9df935b190f4dc1e97eb2601d86d014960643b7
tk <tim_k@posteo.de>
2021-11-22 12:57:00 +0100

    add working example

--- algorithms/ids_lstm_main.py
+++ algorithms/ids_lstm_main.py
@@ -1,17 +1,20 @@
 from algorithms.features.ngram_plus_next_syscall import NgramPlusNextSyscall
-from algorithms.features.ngram_minus_one import NgramMinusOne
+# from algorithms.features.ngram_minus_one import NgramMinusOne
 from algorithms.features.threadID_extractor import ThreadIDExtractor
+from algorithms.features.return_value import ReturnValue
 # from algorithms.features.time_delta_syscalls import TimeDeltaSyscalls
 # from algorithms.features.thread_change_flag import ThreadChangeFlag
 from algorithms.features.syscall_to_int import SyscallToInt
 from algorithms.features.w2v_embedding import W2VEmbedding
-from dataloader.data_preprocessor import DataPreprocessor
-from dataloader.data_loader_2019 import DataLoader
+
 from algorithms.decision_engines.lstm import LSTM
 from algorithms.ids import IDS
-from score_plot import ScorePlot
 
-import pprint
+from dataloader.dataloader_factory import dataloader_factory
+from dataloader.data_preprocessor import DataPreprocessor
+from dataloader.direction import Direction
+
+from pprint import pprint
 
 if __name__ == '__main__':
     """
@@ -20,21 +23,21 @@ if __name__ == '__main__':
     ngram_length = 3
     embedding_size = 4
     thread_aware = True
-    scenario = "CVE-2014-0160"
+    scenario = "Bruteforce_CWE-307"
     scenario_path = f'../../Dataset_old/{scenario}/'
+    # data loader for scenario
+    dataloader = dataloader_factory(scenario_path, direction=Direction.CLOSE)
     syscall_feature_list = [W2VEmbedding(vector_size=embedding_size,
                                          window_size=ngram_length,
                                          epochs=100,
                                          scenario_path=scenario_path,
                                          distinct=False),
                             ThreadIDExtractor(),
-                            SyscallToInt()]
-    stream_feature_list = [NgramPlusNextSyscall(feature_list=[W2VEmbedding],
+                            SyscallToInt(),
+                            ReturnValue()]
+    stream_feature_list = [NgramPlusNextSyscall(feature_list=[W2VEmbedding, ReturnValue],
                                                 thread_aware=thread_aware,
                                                 ngram_length=ngram_length)]
-
-    # data loader for scenario
-    dataloader = DataLoader(scenario_path)
 
     dataprocessor = DataPreprocessor(dataloader,
                                      syscall_feature_list,
@@ -46,24 +49,22 @@ if __name__ == '__main__':
                 distinct_syscalls=distinct_syscalls,
                 epochs=20,
                 batch_size=256,
+                return_value=1,
+                time_delta=0,
+                thread_change_flag=0,
                 force_train=False,
                 model_path=f'Models/{scenario}/')
-                # time_delta=0,
-                # thread_change_flag=0,
-                return_value=1)
 
     # define the used features
     ids = IDS(data_loader=dataloader,
               data_preprocessor=dataprocessor,
-              decision_engine=lstm)
+              decision_engine=lstm,
+              plot_switch=False)
 
     ids.train_decision_engine()
     ids.determine_threshold()
     ids.do_detection()
-    pprint.pprint(ids.get_performance())
+    pprint(ids.performance.get_performance())
 
-    # creating plot
-    plot = ScorePlot(scenario_path=dataloader.scenario_path)
-
-    plot.feed_figure(ids.get_plotting_data())
-    plot.show_plot()
+    ids.plot.feed_figure(ids.get_plotting_data())
+    ids.plot.show_plot()
