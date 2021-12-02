510af49f2ccd009614c6242f4df2f2321e520f9c
tim <tk@tower>
2021-12-02 14:42:22 +0100

    Fix comments

--- algorithms/features/impl/return_value.py
+++ algorithms/features/impl/return_value.py
@@ -29,71 +29,81 @@ class ReturnValue(BaseFeature, metaclass=Singleton):
             'get_dents': 0,
             'send_socket': 0
         }
+        self.write = ['write', 'writev']
+        self.read = ['read', 'readv']
+        self.send_socket = ['sendfile', 'sendmsg']
+        self.recv_socket =  ['recvfrom', 'recv', 'recvmsg']
+        self.get_dents =  ['getdents']
+        self.not_interesting = ['clone', 'getcwd', 'lseek', 'fcntl', 'futex', 'epoll_wait']
+        self.interesting = self.read \
+                           + self.write \
+                           + self.send_socket \
+                           + self.recv_socket \
+                           + self.get_dents
 
     def train_on(self, syscall: Syscall, features: dict):
         """
         save max value of each specified syscall
         """
-        return_value_string = syscall.param('res')
-        send_socket = ['sendfile', 'sendmsg']
-        not_interesting = ['clone', 'getcwd', 'lseek', 'fcntl', 'futex', 'epoll_wait']
-        interesting = send_socket + ["write", "read", "getdents", "recv"]
-        if return_value_string is not None and return_value_string in interesting:
-            try:
-                current_bytes = int(return_value_string)
-                if current_bytes > 1 and current_bytes < 1000000000:
-                    if "write" in syscall.name():
+        if syscall.name() in self.interesting:
+            return_value_string = syscall.param('res')
+            if return_value_string is not None:
+                try:
+                    current_bytes = int(return_value_string)
+                    if syscall.name() in self.write:
                         if current_bytes >= self._max['write']:
                             self._max['write'] = current_bytes
-                    elif syscall.name() in send_socket:
+                    elif syscall.name() in self.send_socket:
                         if current_bytes >= self._max['send_socket']:
                             self._max['send_socket'] = current_bytes
-                    elif "read" in syscall.name():
+                    elif syscall.name() in self.read:
                         if current_bytes >= self._max['read']:
                             self._max['read'] = current_bytes
-                    elif "getdents" in syscall.name():
+                    elif syscall.name() in self.get_dents:
                         if current_bytes >= self._max['get_dents']:
                             self._max['get_dents'] = current_bytes
-                    elif "recv" in syscall.name():
+                    elif syscall.name() in self.recv_socket:
                         # recv msg from socket
                         # bytes received
                         if current_bytes >= self._max['recv_socket']:
                             self._max['recv_socket'] = current_bytes
-                    elif syscall.name() in not_interesting:
+                    elif syscall.name() in self.not_interesting:
                         pass
                     else:
                         print("not handled")
                         print(syscall.name(), return_value_string)
-            except Exception:
-                print('Return Value: Could not cast return value to int')
-                print(f' Return string: {return_value_string}')
+                except Exception as e:
+                    print(e)
+                    print('Return Value: Could not cast return value to int')
+                    print(f' Return string: {return_value_string}')
 
     def extract(self, syscall: Syscall, features: dict):
         """
         extract return value type and normalize with max value of training phase
         """
-        return_value_string = syscall.param('res')
         return_type = None
-        send_socket = ['sendfile', 'sendmsg']
-        not_interesting = ['clone', 'getcwd', 'lseek', 'fcntl', 'futex', 'epoll_wait']
-        interesting = send_socket + ["write", "read", "getdents", "recv"]
         normalized_bytes = 0
-        if return_value_string is not None and return_value_string in interesting:
-            try:
-                current_bytes = int(return_value_string)
-                if current_bytes > 1 and current_bytes < 1000000000:
-                    if "write" in syscall.name():
+        if syscall.name() in self.interesting:
+            return_value_string = syscall.param('res')
+            if return_value_string is not None:
+                try:
+                    current_bytes = int(return_value_string)
+                    if syscall.name() in self.write:
                         return_type = 'write'
-                    elif syscall.name() in send_socket:
+                    elif syscall.name() in self.send_socket:
                         return_type = 'send_socket'
-                    elif "read" in syscall.name():
+                    elif syscall.name() in self.read:
                         return_type = 'read'
-                    elif "recv" in syscall.name():
+                    elif syscall.name() in self.recv_socket:
                         return_type = 'recv_socket'
-                    elif "getdents" in syscall.name():
+                    elif syscall.name() in self.get_dents:
                         return_type = 'get_dents'
                     else:
                         pass
+                except Exception as e:
+                    print(e)
+                    print('Return Value: Could not cast return value to int')
+                    print(f' Return string: {return_value_string}')
                 try:
                     if return_type:
                         normalized_bytes = current_bytes/self._max[return_type]
@@ -101,9 +111,6 @@ class ReturnValue(BaseFeature, metaclass=Singleton):
                         normalized_bytes = 0
                 except ZeroDivisionError:
                     normalized_bytes = 0
-            except Exception:
-                print('Return Value: Could not cast return value to int')
-                print(f' Return string: {return_value_string}')
         features[self.get_id()] = normalized_bytes
 
     def depends_on(self):
