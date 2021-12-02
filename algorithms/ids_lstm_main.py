624f4d087a393926efeb50accff03f59784fdb48
tim <tk@tower>
2021-12-02 09:49:51 +0100

    also added ids_lstm new version

--- algorithms/ids_lstm_main.py
+++ algorithms/ids_lstm_main.py
@@ -1,15 +1,17 @@
-from algorithms.features.ngram_plus_next_syscall import NgramPlusNextSyscall
-from algorithms.features.ngram_minus_one import NgramMinusOne
-from algorithms.features.threadID import ThreadID
 # from algorithms.features.time_delta_syscalls import TimeDeltaSyscalls
-# from algorithms.features.thread_change_flag import ThreadChangeFlag
-from algorithms.features.int_embedding import IntEmbedding
-from algorithms.features.w2v_embedding import W2VEmbedding
-from dataloader.data_preprocessor import DataPreprocessor
+from algorithms.features.impl.thread_change_flag import ThreadChangeFlag
+from algorithms.features.impl.ngram_minus_one import NgramMinusOne
+from algorithms.features.impl.int_embedding import IntEmbedding
+from algorithms.features.impl.w2v_embedding import W2VEmbedding
+from algorithms.features.impl.return_value import ReturnValue
+from algorithms.features.impl.threadID import ThreadID
+from algorithms.features.impl.ngram import Ngram
+
 from dataloader.data_loader_2019 import DataLoader
+
 from algorithms.decision_engines.lstm import LSTM
+
 from algorithms.ids import IDS
-from score_plot import ScorePlot
 
 from pprint import pprint
 
@@ -22,23 +24,32 @@ if __name__ == '__main__':
     thread_aware = True
     scenario = "CVE-2014-0160"
     scenario_path = f'../../Dataset_old/{scenario}/'
-    syscall_feature_list = [W2VEmbedding(vector_size=embedding_size,
-                                         window_size=ngram_length,
-                                         epochs=100,
-                                         scenario_path=scenario_path,
-                                         distinct=False),
-                            ThreadID(),
-                            IntEmbedding()]
-    stream_feature_list = [NgramPlusNextSyscall(feature_list=[W2VEmbedding],
-                                                thread_aware=thread_aware,
-                                                ngram_length=ngram_length)]
 
     # data loader for scenario
-    dataloader = DataLoader(scenario_path)
+    dataloader = dataloader_factory(scenario_path, direction=Direction.CLOSE)
 
-    dataprocessor = DataPreprocessor(dataloader,
-                                     syscall_feature_list,
-                                     stream_feature_list)
+    w2v = W2VEmbedding(
+        vector_size=embedding_size,
+        window_size=10,
+        epochs=5000,
+        scnenario_path=scenario_path,
+        path=f'Models/{scenario}/W2V/',
+        force_train=True,
+        distinct=True,
+        thread_aware=True
+    )
+    return_value = ReturnValue()
+    ngram = Ngram(
+        feature_list=[w2v],
+        thread_aware=thread_aware,
+        ngram_length=ngram_length
+    )
+    ngram_minus_one = NgramMinusOne(
+        ngram,
+        embedding_size
+    )
+    int_embedding = IntEmbedding()
+
     # decision engine (DE)
     distinct_syscalls = dataloader.distinct_syscalls_training_data()
     lstm = LSTM(ngram_length=ngram_length,
@@ -47,14 +58,16 @@ if __name__ == '__main__':
                 epochs=20,
                 batch_size=256,
                 force_train=False,
-                model_path=f'Models/{scenario}/')
-                # time_delta=0,
-                # thread_change_flag=0,
-                # return_value=0)
+                model_path=f'Models/{scenario}/',
+                time_delta=0,
+                thread_change_flag=1,
+                return_value=1)
 
     # define the used features
     ids = IDS(data_loader=dataloader,
-              data_preprocessor=dataprocessor,
+              feature_list=[int_embedding,
+                            ngram_minus_one,
+                            thread_change_flag],
               decision_engine=lstm,
               plot_switch=True)
 
@@ -62,7 +75,3 @@ if __name__ == '__main__':
     ids.determine_threshold()
     ids.do_detection()
     pprint(ids.performance.get_performance())
-
-    ids.plot.feed_figure()
-    ids.plot.show_plot()
-    
