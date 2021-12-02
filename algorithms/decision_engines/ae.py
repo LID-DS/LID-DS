f7d15add754853c454f990c8391e04531242abec
Martin Grimmer <mgrimmer42@gmail.com>
2021-12-02 16:38:34 +0100

    wip AE

---
+++ algorithms/decision_engines/ae.py
@@ -0,0 +1,85 @@
+from algorithms.decision_engines.base_decision_engine import BaseDecisionEngine
+import torch
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+
+# https://www.geeksforgeeks.org/implementing-an-autoencoder-in-pytorch/
+
+class AENetwork(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self._input_size = input_size
+        self._hidden_size = hidden_size
+        
+        # Building an linear encoder with Linear
+        # layer followed by Relu activation function
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(self._input_size, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, self._hidden_size)
+        )
+          
+        # Building an linear decoder with Linear
+        # layer followed by Relu activation function
+        # The Sigmoid activation function
+        # outputs the value between 0 and 1
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(self._hidden_size, 64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, self._input_size),
+            torch.nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+class AE(BaseDecisionEngine):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self._nn = AENetwork(input_size,hidden_size)
+        self._loss_function = torch.nn.MSELoss()
+        self._epochs = 10
+        self._training_data = []
+        self._optimizer = torch.optim.Adam(            
+            self._nn.parameters(),
+            lr = 1e-1,
+            weight_decay=1e-8
+        )
+
+    def train_on(self, input_array: list):
+        x = np.array(input_array)        
+        self._training_data.append(x)
+        
+    def val_on(self, input_array: list):
+        x = np.array(input_array)
+        self._val_data.append(x)
+
+    def fit(self):
+        # tqdm(range(self._epochs), 'training network:'.rjust(25), unit=" epochs"):
+        for epoch in tqdm(range(self._epochs), 'training network:'.rjust(25), unit=" epochs"):
+            for input_vector in self._training_data:
+                # Output of Autoencoder
+                ae_output = self._nn(input_vector)                    
+                # Calculating the loss function
+                loss = self._loss_function(ae_output, input_vector)
+                    
+                # The gradients are set to zero,
+                # the the gradient is computed and stored.
+                # .step() performs parameter update
+                self._optimizer.zero_grad()
+                loss.backward()
+                self._optimizer.step()
+
+    def predict(self, input_array: list) -> float:
+        # Output of Autoencoder
+        ae_output = self._nn(input_array)                    
+        # Calculating the loss function
+        loss = self._loss_function(ae_output, input_array)
+        return loss
+
+    def new_recording(self):
+        pass
