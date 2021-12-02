e2b9f6c5927b82aaf02cc32748fe15267ea27fdd
boneymoy88 <38049744+boneymoy88@users.noreply.github.com>
2021-12-02 15:05:58 +0100

    #243 return value (#254)
    * introduced thread change flag  which needs changes in data_processor
    
    * initial commit
    
    * actually pretty shitty code
    
    * minor changes
    
    * finished return_value feature and added test
    
    * add working example
    
    * clean pull-request
    
    * introduce return value feature and fic thread_change_flag
    
    * change main for new graph way of LIDS
    
    * Updated to new version
    
    * fixed no feedback to exception
    
    * only trying to access return values of interesting syscalls
    
    * Fix comments
    
    Co-authored-by: tim <tk@tower>

--- algorithms/features/impl/test/test_thread_change_flag.py
+++ algorithms/features/impl/test/test_thread_change_flag.py
@@ -1,19 +1,20 @@
-import pytest
-
 from algorithms.features.impl.ngram import Ngram
 from algorithms.features.impl.threadID import ThreadID
 from algorithms.features.impl.thread_change_flag import ThreadChangeFlag
 from algorithms.features.impl.syscall_name import SyscallName
-from dataloader.syscall import Syscall
+from dataloader.syscall_2021 import Syscall2021 as Syscall
 
 
 def helper(syscall, feature_list, ngram, tcf, cid):
     syscall_dict = {}
     for feature in feature_list:
         feature.extract(syscall, syscall_dict)
-    ngram.extract(None, syscall_dict)
-    tcf.extract(None, syscall_dict)
+    ngram.extract(syscall, syscall_dict)
+    tcf.extract(syscall, syscall_dict)
+    print(syscall_dict)
+    print(ngram.get_id())
     return syscall_dict[cid]
+
 
 def test_thread_change_flag():
     # legit
@@ -56,55 +57,44 @@ def test_thread_change_flag():
     syscall_10 = Syscall(
         "1631209047762064269 0 3686303 apache2 3686303 hello < fd=53(<4t>172.19.0.1:36368->172.19.0.3:3306) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
 
-    syscalls = [syscall_1,
-                syscall_2,
-                syscall_3,
-                syscall_4,
-                syscall_5,
-                syscall_6,
-                syscall_7,
-                syscall_8,
-                syscall_9,
-                syscall_10]
-
     features = [ThreadID(), SyscallName()]
 
     ng = Ngram(
-        feature_list=[SyscallName],
+        feature_list=[SyscallName()],
         thread_aware=True,
         ngram_length=3
     )
 
-    tcf = ThreadChangeFlag()
+    tcf = ThreadChangeFlag(ng)
 
     id = tcf.get_id()
 
     # SYSCALL 1
-    assert helper(syscall[0], features, ng, tcf, id) == None
+    assert helper(syscall_1, features, ng, tcf, id) == 0
 
     # SYSCALL 2
-    assert helper(syscall[1], features, ng, tcf, id) == None
+    assert helper(syscall_2, features, ng, tcf, id) == 0
 
     # SYSCALL 3
-    assert helper(syscall[2], features, ng, tcf, id) == ['open', 'close', 'poll', 0]
+    assert helper(syscall_3, features, ng, tcf, id) == 1
 
     # SYSCALL 4
-    assert helper(syscall[3], features, ng, tcf, id) == None
+    assert helper(syscall_4, features, ng, tcf, id) == 0
 
     # SYSCALL 5
-    assert helper(syscall[4], features, ng, tcf, id) == None
+    assert helper(syscall_5, features, ng, tcf, id) == 0
 
     # SYSCALL 6
-    assert helper(syscall[5], features, ng, tcf, id) == None
+    assert helper(syscall_6, features, ng, tcf, id) == 0
 
     # SYSCALL 7
-    assert helper(syscall[6], features, ng, tcf, id) == ['close', 'poll', 'mmap', 0]
+    assert helper(syscall_7, features, ng, tcf, id) == 0
 
     # SYSCALL 8
-    assert helper(syscall[7], features, ng, tcf, id) == ['mmap', 'open', 'open', 1]
+    assert helper(syscall_8, features, ng, tcf, id) == 1
 
     # SYSCALL 9
-    assert helper(syscall[8], features, ng, tcf, id) == ['poll', 'mmap', 'close', 1]
+    assert helper(syscall_9, features, ng, tcf, id) == 1
 
     # SYSCALL 10
-    assert helper(syscall[9], features, ng, tcf, id) == ['mmap', 'close', 'hello', 0]
+    assert helper(syscall_10, features, ng, tcf, id) == 0
--- algorithms/ids_lstm_main.py
+++ algorithms/ids_lstm_main.py
@@ -1,15 +1,18 @@
-from algorithms.features.ngram_plus_next_syscall import NgramPlusNextSyscall
-from algorithms.features.ngram_minus_one import NgramMinusOne
-from algorithms.features.threadID import ThreadID
+from algorithms.features.impl.ngram_minus_one import NgramMinusOne
+from algorithms.features.impl.int_embedding import IntEmbedding
+from algorithms.features.impl.w2v_embedding import W2VEmbedding
+from algorithms.features.impl.return_value import ReturnValue
+from algorithms.features.impl.threadID import ThreadID
+from algorithms.features.impl.ngram import Ngram
 # from algorithms.features.time_delta_syscalls import TimeDeltaSyscalls
 # from algorithms.features.thread_change_flag import ThreadChangeFlag
-from algorithms.features.int_embedding import IntEmbedding
-from algorithms.features.w2v_embedding import W2VEmbedding
-from dataloader.data_preprocessor import DataPreprocessor
-from dataloader.data_loader_2019 import DataLoader
+
 from algorithms.decision_engines.lstm import LSTM
 from algorithms.ids import IDS
-from score_plot import ScorePlot
+
+from dataloader.dataloader_factory import dataloader_factory
+from dataloader.data_preprocessor import DataPreprocessor
+from dataloader.direction import Direction
 
 from pprint import pprint
 
@@ -20,49 +23,60 @@ if __name__ == '__main__':
     ngram_length = 3
     embedding_size = 4
     thread_aware = True
-    scenario = "CVE-2014-0160"
-    scenario_path = f'../../Dataset_old/{scenario}/'
-    syscall_feature_list = [W2VEmbedding(vector_size=embedding_size,
-                                         window_size=ngram_length,
-                                         epochs=100,
-                                         scenario_path=scenario_path,
-                                         distinct=False),
-                            ThreadID(),
-                            IntEmbedding()]
-    stream_feature_list = [NgramPlusNextSyscall(feature_list=[W2VEmbedding],
-                                                thread_aware=thread_aware,
-                                                ngram_length=ngram_length)]
+    scenario = "CVE-2017-7529"
+    scenario_path = f'../../Dataset/{scenario}/'
 
     # data loader for scenario
-    dataloader = DataLoader(scenario_path)
+    dataloader = dataloader_factory(scenario_path, direction=Direction.CLOSE)
 
-    dataprocessor = DataPreprocessor(dataloader,
-                                     syscall_feature_list,
-                                     stream_feature_list)
+    # embedding
+    w2v = W2VEmbedding(
+        vector_size=embedding_size,
+        window_size=10,
+        epochs=5000,
+        scenario_path=scenario_path,
+        path=f'Models/{scenario}/W2V/',
+        force_train=True,
+        distinct=True,
+        thread_aware=thread_aware
+    )
+    int_embedding = IntEmbedding()
+
+    # extra parameter
+    return_value = ReturnValue()
+
+    # ngrams
+    ngram = Ngram(feature_list=[w2v, return_value],
+                  thread_aware=True,
+                  ngram_length=ngram_length + 1
+                  )
+    ngram_minus_one = NgramMinusOne(ngram=ngram,
+                                    element_size=embedding_size + 1)  # plus 1 for return value
+
     # decision engine (DE)
     distinct_syscalls = dataloader.distinct_syscalls_training_data()
-    lstm = LSTM(ngram_length=ngram_length,
-                embedding_size=embedding_size,
-                distinct_syscalls=distinct_syscalls,
-                epochs=20,
-                batch_size=256,
-                force_train=False,
-                model_path=f'Models/{scenario}/')
-                # time_delta=0,
-                # thread_change_flag=0,
-                # return_value=0)
+    de = LSTM(ngram_length=ngram_length,
+              embedding_size=embedding_size,
+              distinct_syscalls=distinct_syscalls,
+              epochs=20,
+              batch_size=256,
+              return_value=1,
+              time_delta=0,
+              thread_change_flag=0,
+              force_train=False,
+              model_path=f'Models/{scenario}/')
 
     # define the used features
     ids = IDS(data_loader=dataloader,
-              data_preprocessor=dataprocessor,
-              decision_engine=lstm,
-              plot_switch=True)
+              feature_list=[int_embedding, ngram_minus_one],
+              decision_engine=de,
+              plot_switch=False)
 
+    # training
     ids.train_decision_engine()
+    # threshold
     ids.determine_threshold()
+    # detection
     ids.do_detection()
+    # print(results)
     pprint(ids.performance.get_performance())
-
-    ids.plot.feed_figure()
-    ids.plot.show_plot()
-    
