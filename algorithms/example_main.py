32b45a162b8c4568ec29c90c4ac4a7612bb3533a
Martin Grimmer <mgrimmer42@gmail.com>
2021-12-10 11:45:43 +0100

    working on converting the LSTM

--- algorithms/example_main.py
+++ algorithms/example_main.py
@@ -11,42 +11,46 @@ from algorithms.features.impl.ngram import Ngram
 from algorithms.features.impl.path_evilness import PathEvilness
 from algorithms.features.impl.threadID import ThreadID
 from algorithms.features.impl.time_delta import TimeDelta
+from algorithms.features.impl.w2v_embedding import W2VEmbedding
 from algorithms.ids import IDS
 from dataloader.dataloader_factory import dataloader_factory
 from dataloader.direction import Direction
 
 if __name__ == '__main__':
     # dataloader
-    # scenario_path = "/home/grimmer/data/LID-DS-2021/CVE-2017-7529"
-    scenario_path = "/home/grimmer/data/LID-DS-2019/CVE-2017-7529/"
-    # scenario_path = "/home/grimmer/data/LID-DS-2021/Bruteforce_CWE-307"    
+    scenario_path = "/home/grimmer/data/LID-DS-2021/CVE-2017-7529"
+    # scenario_path = "/home/grimmer/data/LID-DS-2019/CVE-2017-7529/"
+
     dataloader = dataloader_factory(scenario_path,direction=Direction.OPEN)
 
     # features
-    syscall_to_int = IntEmbedding()
-    ngram = Ngram(
-        feature_list=[syscall_to_int],
-        thread_aware=True,
-        ngram_length=5
+    embedding_size = 7
+    w2v = W2VEmbedding(
+        vector_size=embedding_size,
+        window_size=10,
+        epochs=10000,
+        scenario_path=scenario_path,
+        path=f'Models/W2V/',
+        force_train=True,
+        distinct=True,
+        thread_aware=True
     )
 
-    pe = PathEvilness(scenario_path=scenario_path)
-
-    vector = Concat([ngram, pe])
+    ngram = Ngram(
+        feature_list=[w2v],
+        thread_aware=True,
+        ngram_length=7
+    )
 
     # decision engine (DE)
-    de = Som(input_vector=vector, epochs=500)
-    # de = Stide(ngram = ngram, window_length=1000)
-
+    de = Som(input_vector=ngram, epochs=500)
+    
     # the IDS
     ids = IDS(data_loader=dataloader,
               resulting_building_block=de,
-              # decision_engine=de,
               plot_switch=False)
 
     print("feature preparation done")
-    # training
-    #ids.train_decision_engine()
     # threshold
     ids.determine_threshold()
     # detection
