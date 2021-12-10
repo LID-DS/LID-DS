e314416eaa3828e80988f5e985874d2b9221e855
Felix Nirsberger <felix@nirsberger.de>
2021-12-10 14:06:00 +0100

    fixed wrong indentation, end alarm is now triggered after end of every recording

--- algorithms/ids.py
+++ algorithms/ids.py
@@ -106,9 +106,9 @@ class IDS:
             self._data_preprocessor.new_recording()
             self._decision_engine.new_recording()
 
-        # run end alarm once to ensure that last alarm gets saved
-        if self.performance.alarms is not None:
-            self.performance.alarms.end_alarm()
+            # run end alarm once to ensure that last alarm gets saved
+            if self.performance.alarms is not None:
+                self.performance.alarms.end_alarm()
 
     def draw_plot(self):
         # plot data if wanted
