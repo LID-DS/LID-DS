1e9c8b25d7a243ea9ef8c7dbfec15a931614e7df
boneymoy88 <38049744+boneymoy88@users.noreply.github.com>
2021-12-21 12:13:03 +0100

    #216 persistiere ergebnisse (#278)
    * first commit - not finished yet
    
    * updated
    
    * move files and finalize
    
    * example_main addition and small fix
    
    * add default folder
    
    * Update persistance.py
    
    Add a print method
    
    Co-authored-by: tim <tk@tower>
    Co-authored-by: boneymoy <tk@nomail.xx>

--- algorithms/example_main.py
+++ algorithms/example_main.py
@@ -11,20 +11,22 @@ from algorithms.ids import IDS
 from dataloader.dataloader_factory import dataloader_factory
 from dataloader.direction import Direction
 
+from algorithms.persistance import save_to_json, load_from_json
+
 if __name__ == '__main__':
     # dataloader
     #scenario_path = "/home/grimmer/data/LID-DS-2021/CVE-2017-7529"
-    #scenario_path = "/home/grimmer/data/LID-DS-2019/CVE-2017-7529/"
-    scenario_path = "/home/grimmer/data/LID-DS-2021/Bruteforce_CWE-307"
+    scenario_path = "/home/grimmer/data/LID-DS-2019/CVE-2017-7529/"
     # scenario_path = "/home/grimmer/Work/LID-DS-2021/ZipSlip"
     dataloader = dataloader_factory(scenario_path,direction=Direction.BOTH)
 
     # features
     syscall_to_int = IntEmbedding()
+    ngram_length = 5
     ngram = Ngram(
         feature_list=[syscall_to_int],
         thread_aware=True,
-        ngram_length=5
+        ngram_length=ngram_length
     )
 
     # decision engine (DE)
@@ -45,6 +47,12 @@ if __name__ == '__main__':
     # detection
     ids.do_detection()
     # print results
-    pprint(ids.performance.get_performance())
+    results = ids.performance.get_performance()
+    pprint(results)
+    results['ngram'] = ngram_length
+    result_path = 'persistent_data/stide.json'
+    save_to_json(results, result_path)
+    result = load_from_json(result_path)
+    pprint(result)
     # draw plot
-    ids.draw_plot()
+    # ids.draw_plot()
