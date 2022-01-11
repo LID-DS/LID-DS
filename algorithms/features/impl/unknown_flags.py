4c4cbaae856bcbb4931194666474c7289648c43c
Martin Grimmer <mgrimmer42@gmail.com>
2022-01-11 12:11:01 +0100

    fixed unknown flags to match BB interfaces

--- algorithms/features/impl/unknown_flags.py
+++ algorithms/features/impl/unknown_flags.py
@@ -1,9 +1,8 @@
-from algorithms.features.base_feature import BaseFeature
-from algorithms.features.util.Singleton import Singleton
 from dataloader.syscall import Syscall
+from algorithms.building_block import BuildingBlock
+from algorithms.util.Singleton import Singleton
 
-
-class UnknownFlags(BaseFeature, metaclass=Singleton):
+class UnknownFlags(BuildingBlock, metaclass=Singleton):
     def __init__(self):
         super().__init__()
         self._flag_dict = {}
@@ -22,7 +21,7 @@ class UnknownFlags(BaseFeature, metaclass=Singleton):
                 self._flag_dict[syscall.name()] = []
                 self._flag_dict[syscall.name()].append(syscall.param('flags'))
 
-    def extract(self, syscall: Syscall, features: dict):
+    def calculate(self, syscall: Syscall, features: dict):
         """
             lookup of syscall flag in know flags
             if unknown -> returns 1 else 0
