3c5694aabddc33863f93d0cad36fed9fdda32578
boneymoy <tk@nomail.xx>
2022-02-23 16:47:28 +0100

    added return statistic

--- tools/return_value_statistics.py
+++ tools/return_value_statistics.py
@@ -1,13 +1,27 @@
-from dataloader.data_loader_2019 import DataLoader
 from dataloader.recording_2019 import RecordingDataParts
 from dataloader.syscall import Direction
+from dataloader.dataloader_factory import dataloader_factory
 
-from pprint import pprint
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from brokenaxes import brokenaxes
 from tqdm import tqdm
 
 
 import json
 import os
+
+from enum import IntEnum
+
+
+class Plot(IntEnum):
+    WRITE = 1
+    READ = 2
+    SOCKET_SEND = 3
+    SOCKET_RECV = 4
+    DENTS = 5
 
 
 def save_to_json(results: dict, scenario_name: str):
@@ -26,34 +40,28 @@ def calc_return_value_stats(recording_list, description):
     exploit_count = 0
     result_dict = {
         'normal': {
-            'bytes_written': 0,
-            'bytes_read': 0,
-            'bytes_socket': 0,
-            'bytes_kernel': 0
+            'written': [],
+            'read': [],
+            'socket_send': [],
+            'socket_recv': [],
+            'dents': []
         },
         'exploit': {
-            'bytes_written': 0,
-            'bytes_read': 0,
-            'bytes_socket': 0,
-            'bytes_kernel': 0
+            'written': [],
+            'read': [],
+            'socket_send': [],
+            'socket_recv': [],
+            'dents': []
         }
     }
-    read = ['read', 'getdents']
+    write = ['pwrite', 'write', 'writev']
+    read = ['pread', 'read', 'readv']
+    send_socket = ['sendfile', 'sendmsg']
+    recv_socket =  ['recvfrom', 'recv', 'recvmsg']
+    get_dents =  ['getdents']
+    not_interesting = ['clone', 'getcwd', 'lseek', 'fcntl', 'futex', 'epoll_wait']
+    error_codes = ['EAGAIN', 'EINVAL', 'ECONNRESET']
     for recording in tqdm(recording_list, description, unit=" recordings", smoothing=0):
-        rec_stats = {
-            'counter': {
-                'written': 0,
-                'read': 0,
-                'socket': 0,
-                'kernel': 0
-            },
-            'bytes': {
-                'written': 0,
-                'read': 0,
-                'kernel': 0,
-                'socket': 0
-            }
-        }
         if recording.recording_data_list[RecordingDataParts.IS_EXECUTING_EXPLOIT] == 'True':
             exploit_count += 1
             rec = 'exploit'
@@ -66,72 +74,25 @@ def calc_return_value_stats(recording_list, description):
                 try:
                     return_value_int = int(return_value_string)
                     if return_value_int > 10 and return_value_int < 10000000000:
-                        if 'write' in syscall.name():
-                            rec_stats['bytes']['written'] += return_value_int
-                            rec_stats['counter']['written'] += 1
-                        elif any(string in syscall.name() for string in read):
-                            rec_stats['bytes']['read'] += return_value_int
-                            rec_stats['counter']['read'] += 1
-                        elif 'send' in syscall.name():
-                            rec_stats['bytes']['kernel'] += return_value_int
-                            rec_stats['counter']['kernel'] += 1
-                        elif 'recv' in syscall.name():
-                            rec_stats['bytes']['socket'] += return_value_int
-                            rec_stats['counter']['socket'] += 1
-                        elif 'clone' in syscall.name():
-                            # returns threadID
-                            continue
-                        elif 'futex' in syscall.name():
-                            # not returning bytes
-                            continue
-                        elif 'lseek' in syscall.name():
-                            # returns file descriptor offset
-                            continue
-                        elif 'fcntl' in syscall.name():
-                            # returns flags or file descriptor
-                            continue
-                        elif 'getcwd' in syscall.name():
-                            # returns pointer to directory string
-                            continue
-                        elif 'brk' in syscall.name():
-                            # returns success or errno
-                            continue
+                        if syscall.name() in write:
+                            result_dict[rec]['written'].append(return_value_int)
+                        elif syscall.name() in read:
+                            result_dict[rec]['read'].append(return_value_int)
+                        elif syscall.name() in send_socket:
+                            result_dict[rec]['socket_send'].append(return_value_int)
+                        elif syscall.name() in recv_socket:
+                            result_dict[rec]['socket_recv'].append(return_value_int)
+                        elif syscall.name() in get_dents:
+                            result_dict[rec]['dents'].append(return_value_int)
+                        elif syscall.name() in not_interesting:
+                            pass
                         else:
                             print(syscall.name(), return_value_int)
-                except Exception:
-                    pass
-        try:
-            result_dict[rec]['bytes_read'] += rec_stats['bytes']['read'] / rec_stats['counter']['read']
-        except ZeroDivisionError:
-            result_dict[rec]['bytes_read'] += 0
-        try:
-            result_dict[rec]['bytes_written'] += rec_stats['bytes']['written'] / rec_stats['counter']['written']
-        except ZeroDivisionError:
-            result_dict[rec]['bytes_written'] += 0
-        try:
-            result_dict[rec]['bytes_kernel'] += rec_stats['bytes']['kernel'] / rec_stats['counter']['kernel']
-        except ZeroDivisionError:
-            result_dict[rec]['bytes_kernel'] += 0
-        try:
-            result_dict[rec]['bytes_socket'] += rec_stats['bytes']['socket'] / rec_stats['counter']['socket']
-        except ZeroDivisionError:
-            result_dict[rec]['bytes_socket'] += 0
-    if exploit_count == 0:
-        exploit_count = 1
-    result_dict = {
-        'normal': {
-            'bytes_written': int(result_dict['normal']['bytes_written']/normal_count),
-            'bytes_read': int(result_dict['normal']['bytes_read']/normal_count),
-            'bytes_socket': int(result_dict['normal']['bytes_socket']/normal_count),
-            'bytes_kernel': int(result_dict['normal']['bytes_kernel']/normal_count)
-        },
-        'exploit': {
-            'bytes_written': int(result_dict['exploit']['bytes_written']/exploit_count),
-            'bytes_read': int(result_dict['exploit']['bytes_read']/exploit_count),
-            'bytes_socket': int(result_dict['exploit']['bytes_socket']/exploit_count),
-            'bytes_kernel': int(result_dict['exploit']['bytes_kernel']/exploit_count)
-        }
-    }
+                except ValueError:
+                    if any(error in return_value_string for error in error_codes):
+                        # error code was returned so ValueError is expected
+                        # in extraction -1 is returned
+                        pass
     return result_dict
 
 
@@ -140,24 +101,24 @@ if __name__ == '__main__':
     SCENARIO_NAMES = [
         # "Bruteforce_CWE-307",
         # "CVE-2012-2122",
-        # "CVE-2014-0160",
-        # "CVE-2017-7529",
+        # "CVE-2014-0160"
+        "CVE-2017-7529",
         # "CVE-2018-3760",
         # "CVE-2019-5418",
         # "PHP_CWE-434",
-        "EPS_CWE-434",
-        "ZipSlip"
+        # "EPS_CWE-434",
+        # "ZipSlip"
     ]
     # iterates through list of all scenarios, main loop
     for scenario in SCENARIO_NAMES:
         # scenario = 'CVE-2017-7529'
-        dataloader = DataLoader(f'../../Dataset_old/{scenario}/', Direction.CLOSE)
+        dataloader = dataloader_factory(f'../../Dataset/{scenario}/', Direction.CLOSE)
         result_dict = {}
 
         # dict to describe dataset structure
         data_parts = {
-            'Training': dataloader.training_data(),
-            'Validation': dataloader.validation_data(),
+            # 'Training': dataloader.training_data()
+            # 'Validation': dataloader.validation_data(),
             'Test': dataloader.test_data()
         }
         for data_part in data_parts.keys():
@@ -166,5 +127,34 @@ if __name__ == '__main__':
                 result_dict[scenario] = {}
             result_dict[scenario][data_part] = record_results
 
-        pprint(result_dict)
-        save_to_json(result_dict, scenario)
+        data_dict = {
+            'written': [],
+            'read': [],
+            'socket_send': [],
+            'socket_recv': [],
+            'dents': []
+        }
+        for scenario in result_dict.keys():
+            for rec_type in result_dict[scenario]['Test'].keys():
+                for byte_type in result_dict[scenario]['Test'][rec_type].keys():
+                    data_dict[byte_type].append(pd.DataFrame(result_dict[scenario]['Test'][rec_type][byte_type]))
+        for entry in data_dict:
+            df_normal = data_dict[entry][0]
+            df_exploit = data_dict[entry][1]
+            ax1 = plt.subplot(211)
+            plt.title(f'{entry} - System Calls')
+            ax1.hist(df_normal, color='#076678')
+            ax1.legend(['Normalverhalten'], loc='upper right')
+            ax1.set_ylabel('Occurences')
+            ax2 = plt.subplot(212, sharex=ax1)
+            ax2.hist(df_exploit, color='#9d0006')
+            ax2.legend(['Normal- und Angriffsverhalten'], loc='upper right')
+            ax2.set_ylabel('Occurences')
+            ax2.set_xlabel('Bytes')
+            plt.savefig(f'images/fix{scenario}{entry}.jpg', dpi=200)
+            plt.figure()
+        # plot.show()
+        #fig = px.histogram(df)
+        # fig.wirte_image('images/test_img.svg')
+        #fig.show()
+        #save_to_json(result_dict, scenario)
