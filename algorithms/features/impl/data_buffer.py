ed011ba1cf1948dfa234f4cf9df3b91ef0cf3bcf
boneymoy88 <38049744+boneymoy88@users.noreply.github.com>
2022-06-07 14:35:06 +0200

    331 data buffer do not decode (#338)
    * now working for all BBs and defaults to syscall_name
    
    * prepare pull request
    
    * decoding base64 string by default
    
    * fixing entropy test using data_buffer
    
    * fix pull request
    
    * mergimerg
    
    * last ever commit
    
    * git hell
    
    * fix entropy test
    
    Co-authored-by: tim <tk@tower>
    Co-authored-by: tim <tk-whk@tk.misc.intern.uni-leipzig.de>

--- algorithms/features/impl/data_buffer.py
+++ algorithms/features/impl/data_buffer.py
@@ -1,23 +1,28 @@
-import typing
+import base64
 
-from algorithms.building_block import BuildingBlock
-from algorithms.util.Singleton import Singleton
 from dataloader.syscall import Syscall
 from dataloader.syscall_2019 import Syscall2019
-from dataloader.syscall_2021 import Syscall2021
+
+from algorithms.building_block import BuildingBlock
 
 
-class DataBuffer(BuildingBlock, metaclass=Singleton):
+class DataBuffer(BuildingBlock):
 
-    def __init__(self):
+    def __init__(self, decode: bool = True):
         super().__init__()
+        self._decode = decode
 
     def _calculate(self, syscall: Syscall):
         """
         extract data buffer of syscall
         """
         data_buffer = syscall.param(param_name='data')
-        return data_buffer 
+        if data_buffer is not None:
+            if type(syscall) is not Syscall2019:
+                if self._decode:
+                    data_buffer = base64.b64decode(data_buffer)
+                    data_buffer = str(data_buffer)
+        return data_buffer
 
     def depends_on(self):
         return []
--- algorithms/features/impl/test/test_entropy.py
+++ algorithms/features/impl/test/test_entropy.py
@@ -55,5 +55,5 @@ def test_entropy():
     assert ent._calculate(syscall_2) is None              # No data buffer
     assert ent._calculate(syscall_3) is None              # No data buffer
     assert round(ent._calculate(syscall_4), 2) == 3.94
-    assert round(ent._calculate(syscall_5), 2) == 5.42
-    assert round(ent._calculate(syscall_6), 2) == 5.37
+    assert round(ent._calculate(syscall_5), 2) == 4.03
+    assert round(ent._calculate(syscall_6), 2) == 4.02
