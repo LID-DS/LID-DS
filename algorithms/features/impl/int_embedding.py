3b5359e4eebf5df7940c6650716c8c92e07afcda
tim <tk@tower>
2022-06-07 10:31:02 +0200

    now working for all BBs and defaults to syscall_name

--- algorithms/features/impl/int_embedding.py
+++ algorithms/features/impl/int_embedding.py
@@ -1,21 +1,23 @@
-import typing
-
-from algorithms.building_block import BuildingBlock
-from algorithms.util.Singleton import Singleton
 from dataloader.syscall import Syscall
 
+from algorithms.building_block import BuildingBlock
+from algorithms.features.impl.syscall_name import SyscallName
 
-class IntEmbedding(BuildingBlock, metaclass=Singleton):
+
+class IntEmbedding(BuildingBlock):
     """
         convert system call name to unique integer
     """
 
-    def __init__(self):
+    def __init__(self, building_block: BuildingBlock = None):
         super().__init__()
         self._syscall_dict = {}
+        if building_block is None:
+            building_block = SyscallName()
+        self._dependency_list = [building_block]
 
     def depends_on(self):
-        return []
+        return self._dependency_list
 
     def train_on(self, syscall: Syscall):
         """
@@ -23,15 +25,18 @@ class IntEmbedding(BuildingBlock, metaclass=Singleton):
             integer is current length of syscall_dict
             keep 0 free for unknown syscalls
         """
-        if syscall.name() not in self._syscall_dict:
-            self._syscall_dict[syscall.name()] = len(self._syscall_dict) + 1
+        bb_value = self._dependency_list[0].get_result(syscall)
+        if bb_value not in self._syscall_dict:
+            self._syscall_dict[bb_value] = len(self._syscall_dict) + 1
 
     def _calculate(self, syscall: Syscall):
         """
-            transforms given syscall name to integer
+            transforms given building_block to integer
         """
+        bb_value = self._dependency_list[0].get_result(syscall)
+        print(bb_value)
         try:
-            sys_to_int = self._syscall_dict[syscall.name()]
+            sys_to_int = self._syscall_dict[bb_value]
         except KeyError:
             sys_to_int = 0
         return sys_to_int
