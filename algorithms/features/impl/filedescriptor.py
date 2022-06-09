8d65172381dad602cf5c4bbf1772a53c0520a81a
Felix Nirsberger <felix@nirsberger.de>
2022-06-09 15:35:04 +0200

    first working version and test
    not sure if this is the correct behaviour regarding tuples with fds

--- algorithms/features/impl/filedescriptor.py
+++ algorithms/features/impl/filedescriptor.py
@@ -1,13 +1,14 @@
-from enum import Enum
+import re
+from enum import IntEnum
 
 from algorithms.building_block import BuildingBlock
 from algorithms.util.Singleton import Singleton
 from dataloader.syscall import Syscall
 
 
-class FDMode(Enum):
+class FDMode(IntEnum):
+    ID = 0
     Content = 1
-    ID = 2
 
 
 class FileDescriptor(BuildingBlock, metaclass=Singleton):
@@ -18,22 +19,34 @@ class FileDescriptor(BuildingBlock, metaclass=Singleton):
 
     def _calculate(self, syscall: Syscall):
         """
-        calculate process ID of syscall
+        calculate file descriptor of syscall
 
         Params:
             syscall(Syscall)
         """
-        if 'fd' in syscall.params():
-            fd = syscall.param('fd')
-            fd_parts = fd[:-1].split('(')
-            if self._mode == FDMode.ID:
-                pass
-            elif self._mode == FDMode.Content:
-                pass
+        params = syscall.params()
+        if 'fd' in params:
+            return self._get_fd_part(syscall.param('fd'), self._mode)
+        elif 'in_fd' in params:
+            if 'out_fd' in params:
+                return self._get_fd_part(syscall.param('in_fd'), self._mode) + self._get_fd_part(
+                    syscall.param('out_fd'), self._mode)
             else:
-                raise ValueError("Invalid Extraction Mode")
+                return self._get_fd_part(syscall.param('in_fd'), self._mode)
+        elif 'out_fd' in params:
+            return self._get_fd_part(syscall.param('in_fd'), self._mode)
         else:
             return None
+
+    @staticmethod
+    def _get_fd_part(fd, mode: FDMode):
+        fd_parts = fd[:-1].split('(')
+        part = fd_parts[mode]
+        pattern = r'<.*>'
+        if '->' in part:
+            return tuple(re.sub(pattern, '', e) for e in part.split('->'))
+        else:
+            return tuple([re.sub(pattern, '', part)])
 
     def depends_on(self):
         return []
---
+++ algorithms/features/impl/test/test_filedescriptor.py
@@ -0,0 +1,81 @@
+import os
+
+from tqdm import tqdm
+
+from algorithms.features.impl.filedescriptor import FileDescriptor, FDMode
+from algorithms.features.impl.syscall_name import SyscallName
+from dataloader.dataloader_factory import dataloader_factory
+from dataloader.syscall_2021 import Syscall2021
+
+
+def test_filedescriptor():
+    # legit
+    syscall_1 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047761484608 0 3686302 apache2 3686302 open < fd=9(<f>/proc/sys/kernel/ngroups_max) name=/proc/sys/kernel/ngroups_max flags=1(O_RDONLY) mode=0 dev=200024")
+
+    # legit
+    syscall_2 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>/proc/sys/kernel/ngroups_min) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # legit
+    syscall_3 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>/etc/group) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # legit with in_fd
+    syscall_4 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < in_fd=9(<f>/etc/test) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # legit with out_fd
+    syscall_5 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < out_fd=9(<f>/etc/password) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # legit
+    syscall_6 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>/proc/sys/kernel/evil) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # no fd
+    syscall_7 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # invalid fd
+    syscall_8 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>gibberish) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # invalid fd
+    syscall_9 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                            "1631209047762064269 0 3686303 apache2 3686303 open < fd=9(<f>wackawacka) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # valid fd but ip instead of file
+    syscall_10 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                             "1631209047762064269 0 3686303 apache2 3686303 open < fd=53(<4t>172.17.0.1:36368->172.17.0.3:3306) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    # valid fd but ip instead of file
+    syscall_11 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                             "1631209047762064269 0 3686303 apache2 3686303 open < fd=53(<4t>172.19.0.1:36368->172.19.0.3:3306) name=/etc/group flags=4097(O_RDONLY|O_CLOEXEC) mode=0 dev=200021 ")
+
+    syscall_12 = Syscall2021('CVE-2017-7529/test/normal_and_attack/acidic_bhaskara_7006.zip',
+                             '1631161014801307269 65534 3384228 nginx 3384228 sendfile > out_fd=9(<4t>172.21.0.3:50122->172.21.0.7:80) in_fd=20(<f>/etc/nginx/html/images/dashboard_full_2.jpg) offset=0 size=250749')
+
+    fd = FileDescriptor(mode=FDMode.Content)
+
+    print(fd.get_result(syscall_1))
+    print(fd.get_result(syscall_12))
+
+
+if __name__ == '__main__':
+    test_filedescriptor()
+    """
+    name_bb = SyscallName()
+
+    path = os.environ['LID_DS_BASE'] + '/LID-DS-2021'
+    scenarios = os.listdir(path)
+
+    for scenario in scenarios:
+        dataloader = dataloader_factory(os.environ['LID_DS_BASE'] + '/LID-DS-2021/' + scenario)
+
+        for recording in tqdm(dataloader.validation_data()):
+            for syscall in recording.syscalls():
+                name = name_bb.get_result(syscall)
+                if name == 'sendfile':
+                    print(syscall.syscall_line)
+    """
