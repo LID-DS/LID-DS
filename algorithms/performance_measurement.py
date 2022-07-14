7a9b73c2506363068b35e816b4477cb247862b7b
tim <tk@tower>
2022-07-14 14:11:03 +0200

    missing new recording in performance class

--- algorithms/performance_measurement.py
+++ algorithms/performance_measurement.py
@@ -98,6 +98,10 @@ class Performance:
             if anomaly_score <= self._threshold:
                 if self.create_alarms:
                     self.alarms.end_alarm()
+
+                if self._cfp_counter_wait_normal is True:
+                    if self._current_cfp_stream_normal > 0:
+                        print(syscall.recording_path)
                 self._cfp_end_normal()
                 self._tn += 1
 
@@ -139,6 +143,9 @@ class Performance:
 
         # ending cfa before new recording starts
         self._cfp_end_exploits()
+        if self._cfp_counter_wait_normal is True:
+            if self._current_cfp_stream_normal > 0:
+                print(recording.metadata()["name"])
         self._cfp_end_normal()
 
         # ending alarm
