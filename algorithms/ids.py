5167a34ec305013aaec1cbd1058ad4db87cf7d9c
Lukas Hempel <lh11jise@studserv.uni-leipzig.de>
2022-07-25 14:36:36 +0200

    Fixed threshold determination

--- algorithms/ids.py
+++ algorithms/ids.py
@@ -54,7 +54,7 @@ class IDS:
                 anomaly_score = self._final_bb.get_result(syscall)
                 if anomaly_score != None:                
                     if anomaly_score > max_score:
-                        max_score = anomaly_score
+                        max_score = deepcopy(anomaly_score)
             self._data_preprocessor.new_recording()            
         self.threshold = max_score
         self.performance.set_threshold(max_score)
