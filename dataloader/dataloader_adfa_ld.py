645c2fbc0a5ba53a1e562c52f30a1969232d7243
Felix Nirsberger <felix@nirsberger.de>
2022-08-19 10:56:38 +0200

    * added attributes for attacks and validation count * added Enum for Attacks

--- dataloader/dataloader_adfa_ld.py
+++ dataloader/dataloader_adfa_ld.py
@@ -1,13 +1,24 @@
+from enum import Enum
+
 from base_data_loader import BaseDataLoader
+
+class Attacks(Enum):
+    Adduser = 1
+    Hydra_FTP = 2
+    Hydra_SSH = 3
+    Java_Meterpreter = 4
+    Meterpreter = 5
+    Web_Shell = 6
 
 
 class DataLoaderADFALD(BaseDataLoader):
-    def __init__(self, scenario_path: str):
+    def __init__(self, scenario_path: str, attack: Attacks, validation_count: int):
         super().__init__(scenario_path)
         self._normal_recordings = None
         self._exploit_recordings = None
         self._distinct_syscalls = None
-        self._direction = direction
+        self._attack = attack
+        self._validation_count = validation_count
 
         self.extract_recordings()
 
