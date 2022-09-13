321025485a2711cd912fde73914cc7604fcf5458
tim <tk-whk@tk.misc.intern.uni-leipzig.de>
2022-09-13 11:18:01 +0200

    update ae file

--- algorithms/ids_ae_main.py
+++ algorithms/ids_ae_main.py
@@ -1,121 +1,123 @@
 import os
 import sys
-import math
-
-import torch
 
 from pprint import pprint
 
 from dataloader.direction import Direction
 from dataloader.dataloader_factory import dataloader_factory
 
+from algorithms.features.impl.stream_sum import StreamSum
+from algorithms.features.impl.syscall_name import SyscallName
+from algorithms.features.impl.one_hot_encoding import OneHotEncoding
+
 from algorithms.ids import IDS
+from algorithms.persistance import save_to_mongo
 
 from algorithms.decision_engines.ae import AE
-from algorithms.decision_engines.aetf import AE_TF
-from algorithms.features.impl.stream_sum import StreamSum
+
 from algorithms.features.impl.ngram import Ngram
-from algorithms.features.impl.one_hot_encoding import OneHotEncoding
-from algorithms.features.impl.syscall_name import SyscallName
 from algorithms.features.impl.w2v_embedding import W2VEmbedding
+
 
 if __name__ == '__main__':
 
-    # lid_ds_version:
-    #    "LID-DS-2019"
-    #    "LID-DS-2021"
+    lid_ds_version_number = 0
+    lid_ds_version = [
+        "LID-DS-2019",
+        "LID-DS-2021"
+    ]
 
-    # scenarios ordered by training data size asc    
-    # 
-    #    "CVE-2017-7529",
-    #    "CVE-2014-0160",
-    #    "CVE-2012-2122",
-    #    "Bruteforce_CWE-307",
-    #    "CVE-2020-23839",
-    #    "CWE-89-SQL-injection",
-    #    "PHP_CWE-434",
-    #    "ZipSlip",
-    #    "CVE-2018-3760",
-    #    "CVE-2020-9484",
-    #    "EPS_CWE-434",
-    #    "CVE-2019-5418",
-    #    "Juice-Shop",
-    #    "CVE-2020-13942",
-    #    "CVE-2017-12635_6"
+    # scenarios ordered by training data size asc
+    # 0 - 14
+    scenario_names = [
+        # "CVE-2017-7529",
+        # "CVE-2014-0160",
+        # "CVE-2012-2122",
+        # "Bruteforce_CWE-307",
+        # "CVE-2020-23839",
+        # "CWE-89-SQL-injection",
+        # "PHP_CWE-434",
+        "ZipSlip",
+        "CVE-2018-3760",
+        # "CVE-2020-9484",
+        "EPS_CWE-434",
+        "CVE-2019-5418",
+        # "Juice-Shop",
+        # "CVE-2020-13942",
+        # "CVE-2017-12635_6"
+    ]
 
+    ###################
     # feature config:
     ngram_length = 7
-    w2v_size = 10
-    w2v_window_size = 10
+    w2v_size = 5
     thread_aware = True
 
-    # getting the LID-DS base path, version and scenario from argument
-    try:        
+    # run config
+    scenario_range = scenario_names[0:1]
+    ###################
+
+    # getting the LID-DS base path from argument or environment variable
+    if len(sys.argv) > 1:
         lid_ds_base_path = sys.argv[1]
-        lid_ds_version = sys.argv[2]
-        scenario_name = sys.argv[3]
-    except:
-        print(f"Error, call with:\n> python3 {sys.argv[0]} lid_ds_base_path lid_ds_version scenario_name")
-        exit()        
+    else:
+        try:
+            lid_ds_base_path = os.environ['LID_DS_BASE']
+        except KeyError:
+            raise ValueError("No LID-DS Base Path given. Please specify as argument or set Environment Variable "
+                             "$LID_DS_BASE")
 
-    scenario_path = f"{lid_ds_base_path}/{lid_ds_version}/{scenario_name}"        
+    for scenario_number in range(0, len(scenario_range)):
+        scenario_path = os.path.join(lid_ds_base_path,
+                                     lid_ds_version[lid_ds_version_number],
+                                     scenario_range[scenario_number])
+        dataloader = dataloader_factory(scenario_path, direction=Direction.OPEN)
+        name = SyscallName()
+        w2v = W2VEmbedding(epochs=500,
+                           word=name,
+                           vector_size=w2v_size,
+                           window_size=ngram_length
+                           )
 
-    dataloader = dataloader_factory(scenario_path, direction=Direction.OPEN)
+        ohe = OneHotEncoding(name)
 
-    # features
-    ###################
-    name = SyscallName()
-    w2v = W2VEmbedding(epochs=500,
-                        word=name,
-                        vector_size=w2v_size,
-                        window_size=w2v_window_size
-                        )
+        ngram = Ngram(feature_list=[ohe],
+                      thread_aware=thread_aware,
+                      ngram_length=ngram_length
+                      )
+        # pytorch impl.
+        ae = AE(
+            input_vector=ngram
+        )
 
-    ohe = OneHotEncoding(name)
+        # keras/tf impl.
+        # ae_tf = AE_TF(
+            # input_vector=ngram,
+            # epochs=10000
+        # )
 
-    ngram = Ngram(feature_list=[ohe],
-                    thread_aware=thread_aware,
-                    ngram_length=ngram_length
-                    )
-    
-    # pytorch impl.
-    ae = AE(
-        input_vector=ngram
-    )
+        sum = StreamSum(ae, False,10)
 
-    # keras/tf impl.
-    ae_tf = AE_TF(
-        input_vector=ngram,
-        epochs=10000
-    )
+        ###################
+        # the IDS
+        ids = IDS(data_loader=dataloader,
+                  resulting_building_block=sum,
+                  create_alarms=False,
+                  plot_switch=False)
 
+        print("at evaluation:")
+        # threshold
+        ids.determine_threshold()
+        # detection
+        results = ids.detect().get_results()
+        pprint(results)
 
-    sum = StreamSum(ae_tf, False,10)
-
-    ###################
-    # the IDS
-    ids = IDS(data_loader=dataloader,
-                resulting_building_block=sum,
-                create_alarms=False,
-                plot_switch=True)
-
-    print("at evaluation:")
-    # threshold
-    ids.determine_threshold_and_plot()
-    # detection
-    # performance = ids.detect_parallel()
-    performance = ids.detect()
-    results = performance.get_results() 
-    
-    pprint(results)
-    ids.draw_plot()
-    
-
-    # enrich results with configuration and save to disk
-    results['algorithm'] = "AE_TF"
-    results['ngram_length'] = ngram_length
-    results['w2v_size'] = w2v_size
-    results['thread_aware'] = thread_aware
-    results['config'] = ids.get_config()
-    results['scenario'] = scenario_name
-    result_path = 'results/results_ae.json'
+        # enrich results with configuration and save to disk
+        results['algorithm'] = "AE"
+        results['ngram_length'] = ngram_length
+        results['embedding'] = "OHE"
+        results['thread_aware'] = thread_aware
+        results['config'] = ids.get_config()
+        results['scenario'] = scenario_range[scenario_number]
+        results['dataset'] = lid_ds_version[lid_ds_version_number]
+        save_to_mongo(results)
