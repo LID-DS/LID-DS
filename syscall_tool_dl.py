cd6b4fa480d237da41ea5f7e1417b4bb906d6528
mlyfall <ephcs@web.de>
2022-11-22 18:21:25 +0100

    dict with file and ip information using regex

--- syscall_tool_dl.py
+++ syscall_tool_dl.py
@@ -2,6 +2,14 @@ import re
 import json
 from dataloader.dataloader_factory import dataloader_factory
 
+
+def append_feature_dict(feature, dictionary: dict, key: str):
+    if key not in dictionary:
+        dictionary.update({key: feature})
+    elif feature not in dictionary[key]:
+        dictionary[key].append(feature)
+
+
 # data_base = '/home/mly/PycharmProjects/LID-DS-2021/LID-DS-2021'
 # alert_file_path = '/home/mly/PycharmProjects/LID-DS/alarms_n_3_w_100_t_False_LID-DS-2021_CVE-2017-7529.json'
 alert_file_path = '/home/emmely/PycharmProjects/LIDS/Git LIDS/alarms_n_3_w_100_t_False_LID-DS-2021_CVE-2017-7529.json'
@@ -23,8 +31,8 @@ for entry in alert_dict['alarms']:
     syscalls_in_alert = last_line_id - first_line_id
     time_window_seconds = (last_timestamp - first_timestamp) * pow(10, -9)
 
-    ip_pattern = re.compile("^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$")
-    file_path_pattern = re.compile("^/|(/[a-zA-Z0-9_-]+)+$")
+    ip_pattern = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")
+    file_path_pattern = re.compile(r"(\/.*?\.[\w:]+)")
 
     # initialize dict for alert
     alert_analysis_dict = {'scenario_path': scenario_path,
@@ -41,24 +49,21 @@ for entry in alert_dict['alarms']:
             for syscall in recording.syscalls():
                 if syscall.line_id in range(first_line_id, last_line_id + 1):
 
-                    if syscall.user_id() not in alert_analysis_dict['users']:
-                        alert_analysis_dict['users'].append(syscall.user_id())
-
-                    if syscall.process_name() not in alert_analysis_dict['processes']:
-                        alert_analysis_dict['processes'].append(syscall.process_name())
-
-                    if syscall.name() not in alert_analysis_dict['syscall_names']:
-                        alert_analysis_dict['syscall_names'].append(syscall.name())
+                    append_feature_dict(syscall.user_id(), alert_analysis_dict, 'users')
+                    append_feature_dict(syscall.process_name(), alert_analysis_dict, 'processes')
+                    append_feature_dict(syscall.name(), alert_analysis_dict, 'syscall_names')
 
                     if 'fd' in syscall.params():
-                        print(syscall.params()['fd'])
-                        if re.search(file_path_pattern, syscall.params()['fd']):
-                            file = re.search(file_path_pattern, syscall.params()['fd'])
-                            alert_analysis_dict['files'].append(file)
-                        if re.search(ip_pattern, syscall.params()['fd']):
-                            ip = re.search(ip_pattern, syscall.params()['fd'])
-                            alert_analysis_dict['ip'].append(ip)
+                        params_string = syscall.params()['fd']
 
-                    # print(syscall.params())
+                        ip_matches = re.findall(ip_pattern, params_string)
+                        if ip_matches:
+                            for ip in ip_matches:
+                                append_feature_dict(ip, alert_analysis_dict, 'ip')
+
+                        file_matches = re.findall(file_path_pattern, params_string)
+                        if file_matches:
+                            for file in file_matches:
+                                append_feature_dict(file, alert_analysis_dict, 'files')
 
             print(alert_analysis_dict)
