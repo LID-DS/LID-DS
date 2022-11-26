82dbae193b41185912a03b99d6b5e3be4e84d80f
Tinsaye Abye <tinsaye@serlo.org>
2022-11-26 22:33:44 +0100

    [feat, 407] algorithm wise best average configuration query
    This is similar to Table 5 in the paper "Improving Host-based Intrusion Detection Using Thread Information"

--- algorithms/evaluation/experiment_result_queries.py
+++ algorithms/evaluation/experiment_result_queries.py
@@ -6,16 +6,15 @@ import os
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
-from tabulate import tabulate
 
 from dataloader.direction import Direction
 
 
 class ResultQuery:
     def __init__(self, collection_name="experiments"):
-        mongo_ip = os.environ['LID_DS_MONGO_IP']
-        mongo_user = os.environ['LID_DS_MONGO_USER']
-        mongo_pw = os.environ['LID_DS_MONGO_PW']
+        mongo_ip = os.environ["LID_DS_MONGO_IP"]
+        mongo_user = os.environ["LID_DS_MONGO_USER"]
+        mongo_pw = os.environ["LID_DS_MONGO_PW"]
 
         client = MongoClient(
             mongo_ip,
@@ -25,58 +24,27 @@ class ResultQuery:
 
         self._experiments: Collection = client[collection_name][collection_name]
 
-    def get_results(
+    def find_best_algorithm(
             self,
             algorithms,
             scenarios: list[str] = None,
             datasets: list[str] = None,
             directions: list[Direction] = None,
             features: dict[str, list[str]] = None,
-            features_exact_match=False,
+            features_exact_match: bool = False,
             group_by: list[str] = None,
             group_by_config: dict = None,
     ):
         """ Get all results matching the specified arguments. """
 
-        # Match phase for filtering results
-        match = {"algorithm": {"$in": algorithms}}
-        if scenarios is not None:
-            match |= {"scenario": {"$in": scenarios}}
-        if datasets is not None:
-            match |= {"dataset": {"$in": datasets}}
-        if directions is not None:
-            match |= {"direction": {"$in": [d.value for d in directions]}}
-
-        features_query = {
-            "$expr": {
-                "$function": {
-                    "body": _custom_js_function("has_features"),
-                    "args": ["$config.nodes", "$config.links", "$algorithm", features, features_exact_match],
-                    "lang": "js"
-                }
-            }
-        }
-        match |= features_query
-
-        # addFields phase to extract config values
-        config_aliases = [_get_config_aliases([config]) for config in group_by_config.values()]
-        config_aliases = list(itertools.chain.from_iterable(config_aliases))
-
-        addFields = {}
-        for config, config_alias in config_aliases:
-            addFields[config_alias] = {
-                "$function": {
-                    "body": _custom_js_function("get_config_value"),
-                    "args": ["$config.nodes", "$config.links", "$algorithm", group_by_config, config_alias],
-                    "lang": "js"
-                }
-            }
+        match = self.construct_match_stage(algorithms, datasets, directions, features, features_exact_match, scenarios)
+        addFields, config_aliases = self.construct_addFields_stage(group_by_config)
 
         # grouping phase
         if group_by is None:
             group_by = {
-                'dataset': '$dataset',
-                'algorithm': '$algorithm',
+                "dataset": "$dataset",
+                "algorithm": "$algorithm",
             }
 
         group_by |= {alias: f"${alias}" for _, alias in config_aliases}
@@ -110,20 +78,139 @@ class ResultQuery:
 
         # final pipeline
         pipeline = [
-            {
-                "$match": match
-            }, {
-                "$addFields": addFields
-            }, {
-                "$group": group
-            }, {
-                "$sort": sort
+            {"$match": match},
+            {"$addFields": addFields},
+            {"$group": group},
+            {"$sort": sort}
+        ]
+        results = list(self._experiments.aggregate(pipeline))
+        results = [r.pop("_id") | r for r in results]
+        return results
+
+    def algorithm_wise_best_average(
+            self,
+            algorithms,
+            scenarios: list[str] = None,
+            datasets: list[str] = None,
+            directions: list[Direction] = None,
+            features: dict[str, list[str]] = None,
+            features_exact_match: bool = False,
+            group_by: list[str] = None,
+            group_by_config: dict = None,
+            firstK_in_group: int = None,
+    ):
+        """
+            For each algorithm, find the k best configurations
+        """
+        match = self.construct_match_stage(algorithms, datasets, directions, features, features_exact_match, scenarios)
+        addFields, config_aliases = self.construct_addFields_stage(group_by_config)
+
+        # grouping phase
+        if group_by is None:
+            group_by = {
+                'dataset': '$dataset',
+                'algorithm': '$algorithm',
             }
+
+        group_by |= {alias: f"${alias}" for _, alias in config_aliases}
+
+        group = {
+            "_id": group_by,
+            "avg_DR": {
+                "$avg": "$detection_rate"
+            },
+            "avg_FA": {
+                "$avg": {
+                    "$add": ["$consecutive_false_positives_exploits", "$consecutive_false_positives_normal"]
+                }
+            },
+            "sum_FA": {
+                "$sum": {
+                    "$add": ["$consecutive_false_positives_exploits", "$consecutive_false_positives_normal"]
+                }
+            },
+            "count": {
+                "$sum": 1
+            }
+        }
+
+        group2 = {
+            "_id": {
+                "dataset": "$_id.dataset",
+                "algorithm": "$_id.algorithm"
+            },
+            "results": {
+                "$push": "$$ROOT"
+            }
+        }
+
+        sort_algo_wise = {
+            "_id": "$_id",
+            "results": {
+                "$slice": [
+                    {
+                        "$sortArray": {
+                            "input": "$results",
+                            "sortBy": {
+                                "avg_DR": -1,
+                                "avg_FA": 1
+                            }
+                        }
+                    },
+                    0,
+                    firstK_in_group
+                ]
+            }
+        }
+
+        # final pipeline
+        pipeline = [
+            {"$match": match},
+            {"$addFields": addFields},
+            {"$group": group},
+            {"$group": group2},
+            {"$project": sort_algo_wise}
         ]
         result = list(self._experiments.aggregate(pipeline))
-        result = [r | r.pop("_id") for r in result]
-
         return result
+
+    @staticmethod
+    def construct_addFields_stage(group_by_config):
+        """ addFields phase to extract config values """
+        config_aliases = [_get_config_aliases([config]) for config in group_by_config.values()]
+        config_aliases = list(itertools.chain.from_iterable(config_aliases))
+        addFields = {}
+        for config, config_alias in config_aliases:
+            addFields[config_alias] = {
+                "$function": {
+                    "body": _custom_js_function("get_config_value"),
+                    "args": ["$config.nodes", "$config.links", "$algorithm", group_by_config, config_alias],
+                    "lang": "js"
+                }
+            }
+        return addFields, config_aliases
+
+    @staticmethod
+    def construct_match_stage(algorithms, datasets, directions, features, features_exact_match, scenarios):
+        """ Match phase for filtering results """
+        match = {"algorithm": {"$in": algorithms}}
+        if scenarios is not None:
+            match |= {"scenario": {"$in": scenarios}}
+        if datasets is not None:
+            match |= {"dataset": {"$in": datasets}}
+        if directions is not None:
+            match |= {"direction": {"$in": [d.value for d in directions]}}
+        features_query = {
+            "$expr": {
+                "$function": {
+                    "body": _custom_js_function("has_features"),
+                    "args": ["$config.nodes", "$config.links", "$algorithm", features, features_exact_match],
+                    "lang": "js"
+                }
+            }
+        }
+        match |= features_query
+        return match
 
 
 def _custom_js_function(function_name):
@@ -153,82 +240,3 @@ def _get_config_aliases(config_groups: list[dict]) -> list[tuple[str, str]]:
             else:
                 leaves += _get_config_aliases(alias)
     return leaves
-
-
-def main():
-    """ Usage example """
-    features = {
-        "Som": ["Som", "Concat", "Ngram", "IntEmbedding"],
-        "LSTM": ["LSTM", "Ngram", "W2VEmbedding"]
-    }
-    group_by_config = {
-        "Som": {
-            "name": "MaxScoreThreshold",
-            "input": [
-                {
-                    "name": "Som",
-                    "epochs": "som_epochs",
-                    "sigma": "sigma",
-                    "size": "som_size",
-                    "input": [
-                        {
-                            "name": "Concat",
-                            "input": [
-                                {
-                                    "name": "Ngram",
-                                    "thread_aware": "thread_aware",
-                                    "ngram_length": "concat_1_ng_len",
-
-                                },
-                                {
-                                    "name": "Ngram",
-                                    "ngram_length": "concat_2_ng_len",
-                                    "input": [
-                                        {
-                                            "name": "W2VEmbedding",
-                                            "vector_size": "concat_2_w2v_size",
-
-                                        },
-                                        {
-                                            "name": "ReturnValue",
-                                            "min_max_scaling": "rv_minmax"
-
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ],
-                }
-            ],
-        },
-        "LSTM": {
-            "name": "MaxScoreThreshold",
-            "input": [
-                {
-                    "name": "LSTM",
-                    "batch_size": "lstm_batch_size",
-                    "epochs": "epochs",
-                    "input": [
-                        {
-                            "name": "Ngram",
-                            "thread_aware": "thread_aware",
-                        }
-                    ],
-                },
-            ],
-        }
-    }
-    results = ResultQuery(collection_name="experiments3").get_results(
-        algorithms=["Som", "LSTM"],
-        scenarios=["CVE-2017-7529", "CVE-2014-0160"],
-        directions=[Direction.BOTH],
-        features=features,
-        group_by_config=group_by_config,
-    )
-
-    print(tabulate(results, headers="keys", tablefmt='github'))
-
-
-if __name__ == '__main__':
-    main()
