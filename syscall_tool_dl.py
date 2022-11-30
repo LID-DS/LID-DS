aab37d08431d0271d920853110f66466412b6d51
Mlyfall <ephcs@web.de>
2022-11-30 13:57:25 +0100

    Alert class with subclass for processes

--- syscall_tool_dl.py
+++ syscall_tool_dl.py
@@ -2,18 +2,31 @@ import re
 import json
 from dataloader.dataloader_factory import dataloader_factory
 
+ip_pattern = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")
+file_path_pattern = re.compile(r"(\/.*?\.[\w:]+)")
 
-def append_feature_dict(feature, dictionary: dict, key: str):
-    if key not in dictionary:
-        dictionary.update({key: feature})
-    elif feature not in dictionary[key]:
-        dictionary[key].append(feature)
+
+class Alert:
+    def __init__(self, ds_path, time_window, syscall_count):
+        self.path = ds_path
+        self.time_window = time_window
+        self.syscall_count = syscall_count
+        self.process_list = []
+
+    class Process:
+        def __init__(self, process_id, user_id, process_name):
+            self.process_id = process_id
+            self.user_id = user_id
+            self.process_name = process_name
+            self.network_list = []
+            self.files_list = []
 
 
 # data_base = '/home/mly/PycharmProjects/LID-DS-2021/LID-DS-2021'
-# alert_file_path = '/home/mly/PycharmProjects/LID-DS/alarms_n_3_w_100_t_False_LID-DS-2021_CVE-2017-7529.json'
-alert_file_path = '/home/emmely/PycharmProjects/LIDS/Git LIDS/alarms_n_3_w_100_t_False_LID-DS-2021_CVE-2017-7529.json'
-scenario_path = '/mnt/0e52d7cb-afd4-4b49-8238-e47b9089ec68/LID-DS-2021/CVE-2017-7529'
+alert_file_path = '/home/mly/PycharmProjects/LID-DS/alarms_n_3_w_100_t_False_LID-DS-2021_CVE-2017-7529.json'
+# alert_file_path = '/home/emmely/PycharmProjects/LIDS/Git LIDS/alarms_n_3_w_100_t_False_LID-DS-2021_CVE-2017-7529.json'
+# scenario_path = '/mnt/0e52d7cb-afd4-4b49-8238-e47b9089ec68/LID-DS-2021/CVE-2017-7529'
+scenario_path = '/home/mly/PycharmProjects/LID-DS-2021/LID-DS-2021/CVE-2017-7529'
 dataloader = dataloader_factory(scenario_path)
 
 alert_file = open(alert_file_path)
@@ -27,64 +40,44 @@ for entry in alert_dict['alarms']:
     last_timestamp = entry['last_timestamp']
     scenario_path = entry['filepath']
     recording_alert = scenario_path.strip("/'").split('/')[3].strip(".zip")
-
-    syscalls_in_alert = last_line_id - first_line_id
     time_window_seconds = (last_timestamp - first_timestamp) * pow(10, -9)
+    syscalls_in_alert = last_line_id - first_line_id
 
-    ip_pattern = re.compile(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")
-    file_path_pattern = re.compile(r"(\/.*?\.[\w:]+)")
-
-    # initialize dict for alert
-    alert_analysis_dict = {'scenario_path': scenario_path,
-                           'syscalls_in_alert': syscalls_in_alert,
-                           'time_window': time_window_seconds,
-                           'users': [],
-                           'processes': [],
-                           'syscall_names': [],
-                           'files': [],
-                           'ip': []}
-
-    alert_to_json_dict = {"scenarioPath": None,
-                          "timeWindow": None,
-                          "userID": None,
-                          "processes": []
-                          }
-
-    process_dict = {"processID": 666,
-                    "processName": "apache",
-                    "network": [],
-                    "files": []
-                    }
-
-    network_dict = {"clientIP": None,
-                    "clientPort": None,
-                    "serverIP": None,
-                    "serverPort": None
-                    }
-
-    file_dict = {"path": None,
-                 "action": None}
+    alert = Alert(scenario_path, time_window_seconds, syscalls_in_alert)
 
     for recording in dataloader.test_data():
         if recording.name == recording_alert:
             for syscall in recording.syscalls():
                 if syscall.line_id in range(first_line_id, last_line_id + 1):
 
-                    append_feature_dict(syscall.user_id(), alert_analysis_dict, 'users')
-                    append_feature_dict(syscall.process_name(), alert_analysis_dict, 'processes')
-                    append_feature_dict(syscall.name(), alert_analysis_dict, 'syscall_names')
+                    if alert.process_list:
+                        for process in alert.process_list:
+                            if syscall.process_id() not in process.values():
+                                process_inst = alert.Process(syscall.process_id(), syscall.user_id(),
+                                                             syscall.process_name())
+                                alert.process_list.append(vars(process_inst))
+                    else:
+                        process_inst = alert.Process(syscall.process_id(), syscall.user_id(), syscall.process_name())
+                        alert.process_list.append(vars(process_inst))
 
-                    if 'fd' in syscall.params():
-                        params_string = syscall.params()['fd']
+                    if 'fd' in syscall.params():   #same for out/in fd! --> helperfunction?
+                        fd_string = syscall.params()['fd']
+                        if not process_inst.files_list:
+                            file_matches = re.findall(file_path_pattern, fd_string)
+                            if file_matches:
+                                for file in file_matches:
+                                    file_dict = {'path': file,
+                                                 'action': syscall.name()}
+                                    process_inst.files_list.append(file_dict)
 
-                        ip_matches = re.findall(ip_pattern, params_string)
-                        if ip_matches:
-                            for ip in ip_matches:
-                                append_feature_dict(ip, alert_analysis_dict, 'ip')
+                        if not process_inst.network_list:
+                            ip_matches = re.findall(ip_pattern, fd_string)
+                            if ip_matches:
+                                network_dict = {'clientIP': ip_matches[0],
+                                                'clientPort': None,
+                                                'serverIP': ip_matches[1],
+                                                'serverPort': None
+                                                }
 
-                        file_matches = re.findall(file_path_pattern, params_string)
-                        if file_matches:
-                            for file in file_matches:
-                                append_feature_dict(file, alert_analysis_dict, 'files')
-
-            print(alert_analysis_dict)
+    print(vars(alert))
+    break
