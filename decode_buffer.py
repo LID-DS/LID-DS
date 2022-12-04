89f23d1b2775bd4ac68bbfab12ca7588e0e99e11
Mlyfall <ephcs@web.de>
2022-12-04 15:47:31 +0100

    supporting script argument analysis

---
+++ decode_buffer.py
@@ -0,0 +1,36 @@
+from dataloader.dataloader_factory import dataloader_factory
+
+lid_ds_base_path = "/home/mly/PycharmProjects/LID-DS-2021/LID-DS-2021"
+lid_ds_version = "LID-DS-2021"
+
+scenario_names = [
+        "CVE-2017-7529",
+        "CVE-2014-0160",
+        "CVE-2012-2122",
+        "Bruteforce_CWE-307",
+        "CVE-2020-23839",
+        "CWE-89-SQL-injection",
+        "PHP_CWE-434",
+        "ZipSlip",
+        "CVE-2018-3760",
+        "CVE-2020-9484",
+        "EPS_CWE-434",
+        "CVE-2019-5418",
+        "Juice-Shop",
+        "CVE-2020-13942",
+        "CVE-2017-12635_6"
+    ]
+param_names = []
+
+for select_scenario_number in range(0, len(scenario_names)):
+    scenario_path = f"{lid_ds_base_path}/{scenario_names[select_scenario_number]}"
+    dataloader = dataloader_factory(scenario_path)
+
+    for recording in dataloader.test_data():
+        for syscall in recording.syscalls():
+            for key in syscall.params().keys():
+                if key not in param_names:
+                    param_names.append(key)
+
+
+print(param_names)
