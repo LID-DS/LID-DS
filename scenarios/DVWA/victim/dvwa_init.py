2b99eb742a047ab1a98e862cc7507ce96af2ff4f
Felix Nirsberger <felix@nirsberger.de>
2023-02-02 16:42:45 +0100

    added custom DVWA victim fully working including initial login, database creation and security level adjustment

--- scenarios/DVWA/victim/dvwa_init.py
+++ scenarios/DVWA/victim/dvwa_init.py
@@ -1,9 +1,7 @@
-from time import sleep
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from pyvirtualdisplay import Display
 import requests
-import os
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 # dvwa init script - to create the dvwa database
 
@@ -17,25 +15,28 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-browser = webdriver.Chrome(chrome_options=chrome_options)
+browser = webdriver.Chrome(options=chrome_options)
 
 # create DATABASE
+print("creating database...")
 url = 'http://localhost/setup.php'
 browser.get(url)
-browser.find_element_by_name('create_db').click()
+browser.find_element('name', 'create_db').click()
 
 # Login
 url = 'http://localhost/login.php'
+print("logging in...")
 browser.get(url)
-browser.find_element_by_name('username').send_keys('admin')
-browser.find_element_by_name('password').send_keys('password')
-browser.find_element_by_name('Login').click()
+browser.find_element('name', 'username').send_keys('admin')
+browser.find_element('name', 'password').send_keys('password')
+browser.find_element('name', 'Login').click()
 
 # Set security level low
+print("setting security level low...")
 url = 'http://localhost/security.php'
 browser.get(url)
-browser.find_element_by_name('security').send_keys('low')
-browser.find_element_by_name('seclev_submit').click()
+browser.find_element('name', 'security').send_keys('low')
+browser.find_element('name', 'seclev_submit').click()
 
 # shut down
 browser.quit()
