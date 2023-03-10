12e371b1c59a4d3ab0466e15b149f70632f3ddf7
Felix Nirsberger <felix@nirsberger.de>
2023-03-10 10:02:29 +0100

    added MaxScoreThreshold to SOM alarm script

--- algorithms/som_generate_alarms_main.py
+++ algorithms/som_generate_alarms_main.py
@@ -5,6 +5,7 @@ import json
 from pprint import pprint
 
 from algorithms.features.impl.int_embedding import IntEmbedding
+from algorithms.features.impl.max_score_threshold import MaxScoreThreshold
 from algorithms.features.impl.syscall_name import SyscallName
 
 from algorithms.ids import IDS
@@ -19,6 +20,9 @@ from algorithms.features.impl.ngram import Ngram
 from algorithms.features.impl.w2v_embedding import W2VEmbedding
 
 from dataloader.dataloader_factory import dataloader_factory
+
+from dataloader.data_loader_2021 import DataLoader2021
+
 
 if __name__ == '__main__':
 
@@ -35,13 +39,11 @@ if __name__ == '__main__':
         "CVE-2014-0160",
         "CVE-2012-2122",
         "Bruteforce_CWE-307",
-        "CVE-2020-23839",
         "CWE-89-SQL-injection",
         "PHP_CWE-434",
         "ZipSlip",
         "CVE-2018-3760",
         "CVE-2020-9484",
-        "EPS_CWE-434",
         "CVE-2019-5418",
         "Juice-Shop",
         "CVE-2020-13942",
@@ -58,7 +60,7 @@ if __name__ == '__main__':
 
     # run config
     generate_and_write_alarms = True
-    scenario_range = scenario_names[0:15]
+    scenario_range = scenario_names[6:]
     ###################
 
     # getting the LID-DS base path from argument or environment variable
@@ -71,11 +73,11 @@ if __name__ == '__main__':
             raise ValueError("No LID-DS Base Path given. Please specify as argument or set Environment Variable "
                              "$LID_DS_BASE")
 
-    for scenario_number in range(0, len(scenario_range)):
-        scenario_path = os.path.join(lid_ds_base_path,
-                                     lid_ds_version[lid_ds_version_number],
-                                     scenario_range[scenario_number])
-        dataloader = dataloader_factory(scenario_path, direction=Direction.OPEN)
+    for scenario in scenario_range:
+        scenario_path = os.path.join("/media/felix/PortableSSD/datasets/LID-DS-2021",
+                                     scenario)
+        print(scenario_path)
+        dataloader = DataLoader2021(scenario_path, direction=Direction.OPEN)
 
         # features
         ###################
@@ -88,12 +90,14 @@ if __name__ == '__main__':
                            )
         ngram = Ngram([w2v], thread_aware, ngram_length)
         som = Som(ngram, epochs=som_epochs)
-        config_name = f"som_n_{ngram_length}_w_{w2v_size}_e_{som_epochs}_t_{thread_aware}"
+        decider = MaxScoreThreshold(som)
+
+        config_name = f"alarms_SOM_n_{ngram_length}_w_{w2v_size}_e_{som_epochs}_t_{thread_aware}_{scenario}"
 
         ###################
         # the IDS
         ids = IDS(data_loader=dataloader,
-                  resulting_building_block=som,
+                  resulting_building_block=decider,
                   create_alarms=generate_and_write_alarms,
                   plot_switch=False)
 
@@ -101,9 +105,8 @@ if __name__ == '__main__':
         # threshold
         ids.determine_threshold()
         # detection
-        ids.detect_parallel()
         # print results
-        results = ids.performance.get_results()
+        results = ids.detect_parallel().get_results()
         pprint(results)
 
         # enrich results with configuration and save to disk
@@ -112,13 +115,13 @@ if __name__ == '__main__':
         results['w2v_size'] = w2v_size
         results['thread_aware'] = thread_aware
         results['config'] = ids.get_config()
-        results['scenario'] = scenario_range[scenario_number]
+        results['scenario'] = scenario
         result_path = 'results/results_som.json'
         save_to_json(results, result_path)
 
         # alarms - you need the directory "algorithms/results" existing for this to work.
         if generate_and_write_alarms:
             with open(
-                    f"results/alarms_{config_name}_{scenario_range[scenario_number]}.json",
+                    f"results/alarms_{config_name}_{scenario}.json",
                     'w') as jsonfile:
                 json.dump(ids.performance.alarms.get_alarms_as_dict(), jsonfile, default=str, indent=2)
